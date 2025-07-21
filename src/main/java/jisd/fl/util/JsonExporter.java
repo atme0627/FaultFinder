8ded4c765dca9d33b5e71fc1f613a4221e00b32e
atme627 <ezaki.m.aa@m.titech.ac.jp>
2025-07-22 01:20:35 +0900

    minor changes

--- src/main/java/jisd/fl/util/JsonExporter.java
+++ src/main/java/jisd/fl/util/JsonExporter.java
@@ -3,7 +3,6 @@ package jisd.fl.util;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import jisd.fl.probe.info.SuspiciousExpression;
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 
 
 import java.io.File;
@@ -12,7 +11,6 @@ public class JsonExporter {
     public static void exportSuspExpr(SuspiciousExpression root, File output){
         try {
             ObjectMapper mapper = new ObjectMapper();
-            mapper.registerModule(new Jdk8Module());
             mapper.enable(SerializationFeature.INDENT_OUTPUT);
             mapper.writeValue(output, root);
         } catch (Exception e) {
