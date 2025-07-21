d9159da5d1b0dfd9afc00e2b149e5a3eeef20e84
atme627 <ezaki.m.aa@m.titech.ac.jp>
2025-07-21 14:55:15 +0900

    remove FaultFinderForStmt#remove and #susp methods

--- src/main/java/jisd/fl/sbfl/FaultFinderForStmt.java
+++ src/main/java/jisd/fl/sbfl/FaultFinderForStmt.java
@@ -16,43 +16,4 @@ public class FaultFinderForStmt extends FaultFinder{
         super(covForTestSuite, Granularity.LINE, f);
     }
 
-    @Override
-    public void remove(int rank) {
-        ScoreUpdateReport report = new ScoreUpdateReport("REMOVE");
-        FLRankingElement target = flRanking.getElementAtPlace(rank).orElseThrow(
-                () -> new RuntimeException("rank:" + rank + " is out of bounds. (max rank: " + flRanking.getSize() + ")"));
-
-        System.out.println("[  REMOVE  ] " + target);
-        report.recordChange(target);
-
-        target.updateSuspiciousnessScore(0);
-        flRanking.getNeighborElements(target).forEach(e -> {
-            report.recordChange(e);
-            e.updateSuspiciousnessScore(this.removeConst);
-        });
-
-        report.print();
-        flRanking.sort();
-        flRanking.printFLResults(getRankingSize());
-    }
-
-    @Override
-    public void susp(int rank) {
-        ScoreUpdateReport report = new ScoreUpdateReport("SUSP");
-        FLRankingElement target = flRanking.getElementAtPlace(rank).orElseThrow(
-                () -> new RuntimeException("rank: " + rank + " is out of bounds. (max rank: " + flRanking.getSize() + ")"));
-
-        System.out.println("[  SUSP  ] " + target);
-        report.recordChange(target);
-
-        target.updateSuspiciousnessScore(0);
-        flRanking.getNeighborElements(target).forEach(e -> {
-                report.recordChange(e);
-                e.updateSuspiciousnessScore(this.suspConst);
-        });
-
-        report.print();
-        flRanking.sort();
-        flRanking.printFLResults(getRankingSize());
-    }
 }
