ac88e8a7036bd50bb4bedca67b9d5d0dcc40b13c
atme627 <ezaki.m.aa@m.titech.ac.jp>
2025-07-21 21:05:01 +0900

    add nested test classes and JSON export functionality for ProbeForStatement tests

--- src/test/java/jisd/fl/probe/ProbeForStatementTest.java
+++ src/test/java/jisd/fl/probe/ProbeForStatementTest.java
@@ -2,30 +2,68 @@ package jisd.fl.probe;
 
 import jisd.fl.probe.info.SuspiciousExpression;
 import jisd.fl.probe.info.SuspiciousVariable;
+import jisd.fl.util.FileUtil;
+import jisd.fl.util.JsonExporter;
 import jisd.fl.util.PropertyLoader;
 import jisd.fl.util.analyze.MethodElementName;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 class ProbeForStatementTest {
-    @BeforeEach
-    void initProperty() {
-        PropertyLoader.setTargetSrcDir("/Users/ezaki/IdeaProjects/Project4Test/src/main/java");
-        PropertyLoader.setTestSrcDir("/Users/ezaki/IdeaProjects/Project4Test/src/test/java");
+    @Nested
+    class CalcTest {
+        @BeforeEach
+        void initProperty() {
+            PropertyLoader.setTargetSrcDir("/Users/ezaki/IdeaProjects/Project4Test/src/main/java");
+            PropertyLoader.setTestSrcDir("/Users/ezaki/IdeaProjects/Project4Test/src/test/java");
+        }
+
+        @Test
+        void runTest() {
+            SuspiciousVariable target = new SuspiciousVariable(
+                    new MethodElementName("org.sample.CalcTest#methodCall1()"),
+                    "org.sample.CalcTest#methodCall1()",
+                    "actual",
+                    "11",
+                    true,
+                    false
+            );
+
+            ProbeForStatement pfs = new ProbeForStatement(target);
+            SuspiciousExpression treeRoot = pfs.run(2000);
+
+            File output = new File("/Users/ezaki/IdeaProjects/MyFaultFinder/src/test/resources/json/SuspiciousExpression/CalcTest.json");
+            JsonExporter.exportSuspExpr(treeRoot, output);
+        }
     }
 
-    @Test
-    void runTest() {
-        SuspiciousVariable target = new SuspiciousVariable(
-                new MethodElementName("org.sample.CalcTest#methodCall1()"),
-                "org.sample.CalcTest#methodCall1()",
-                "actual",
-                "11",
-                true,
-                false
-        );
+    @Nested
+    class ConditionalTest {
+        @BeforeEach
+        void initProperty() {
+            PropertyLoader.setTargetSrcDir("/Users/ezaki/IdeaProjects/Project4Test/src/main/java");
+            PropertyLoader.setTestSrcDir("/Users/ezaki/IdeaProjects/Project4Test/src/test/java");
+        }
 
-        ProbeForStatement pfs = new ProbeForStatement(target);
-        SuspiciousExpression treeRoot = pfs.run(2000);
+        @Test
+        void runTest() {
+            SuspiciousVariable target = new SuspiciousVariable(
+                    new MethodElementName("org.sample.coverage.ConditionalTest#testXEqualY()"),
+                    "org.sample.coverage.ConditionalTest#testXEqualY()",
+                    "result",
+                    "2",
+                    true,
+                    false
+            );
+
+            ProbeForStatement pfs = new ProbeForStatement(target);
+            SuspiciousExpression treeRoot = pfs.run(2000);
+
+            File output = new File("/Users/ezaki/IdeaProjects/MyFaultFinder/src/test/resources/json/SuspiciousExpression/ConditionalTest.json");
+            JsonExporter.exportSuspExpr(treeRoot, output);
+        }
     }
 }
