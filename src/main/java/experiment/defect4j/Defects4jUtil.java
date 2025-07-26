7cbb5813d48eac69741d1d7afea146a368458f5e
atme627 <ezaki.m.aa@m.titech.ac.jp>
2025-07-26 19:31:35 +0900

    refactor: remove unused methods, redundant imports, and deprecated utilities in Defects4jUtil for improved maintainability

--- src/main/java/experiment/defect4j/Defects4jUtil.java
+++ src/main/java/experiment/defect4j/Defects4jUtil.java
@@ -3,7 +3,7 @@ package experiment.defect4j;
 import io.github.cdimascio.dotenv.Dotenv;
 import jisd.fl.util.PropertyLoader;
 import jisd.fl.util.analyze.LineElementName;
-import jisd.fl.util.analyze.StaticAnalyzer;
+import jisd.fl.util.analyze.MethodElementName;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.lib.ObjectId;
@@ -11,13 +11,10 @@ import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -55,21 +52,13 @@ public class Defects4jUtil {
         return defects4jDir + "/" + project + "/" + project + "_" + bugId + (buggy ? "_buggy" : "_fixed");
     }
 
-    public static File getProjectDir(File rootDir, String project, int bugId, boolean buggy){
-        return new File(rootDir + "/" + project + "/" + project + "_" + bugId + (buggy ? "_buggy" : "_fixed"));
-    }
-
-    @Deprecated
     public static void changeTargetVersion(String project, int bugId){
-        changeTargetVersion(defects4jDir, project, bugId);
-    }
-    public static void changeTargetVersion(File rootDir, String project, int bugId){
-        Properties p = getD4jProperties(rootDir, project, bugId);
+        Properties p = getD4jProperties(project, bugId);
 
-        String targetSrcDir = getProjectDir(rootDir, project, bugId, true) + "/" + p.getProperty("d4j.dir.src.classes");
-        String testSrcDir = getProjectDir(rootDir, project, bugId, true) + "/" + p.getProperty("d4j.dir.src.tests");
-        String targetBinDir = getProjectDir(rootDir, project, bugId, true) + "/target/classes";
-        String testBinDir = getProjectDir(rootDir, project, bugId, true) + "/target/test-classes";
+        String targetSrcDir = getProjectDir(project, bugId, true) + "/" + p.getProperty("d4j.dir.src.classes");
+        String testSrcDir = getProjectDir(project, bugId, true) + "/" + p.getProperty("d4j.dir.src.tests");
+        String targetBinDir = getProjectDir(project, bugId, true) + "/target/classes";
+        String testBinDir = getProjectDir(project, bugId, true) + "/target/test-classes";
 
         PropertyLoader.setProperty("targetSrcDir", targetSrcDir);
         PropertyLoader.setProperty("testSrcDir", testSrcDir);
@@ -78,89 +67,26 @@ public class Defects4jUtil {
         PropertyLoader.store();
     }
 
-    public static List<String> getFailedTestMethods(String project, int bugId){
+    public static List<MethodElementName> getFailedTestMethods(String project, int bugId){
         Properties properties = getD4jProperties(project, bugId);
         String raw = properties.getProperty("d4j.tests.trigger");
-        List<String> failedTests = new ArrayList<>(List.of(raw.split(",")));
-        failedTests = failedTests.stream()
+
+        return new ArrayList<>(List.of(raw.split(",")))
+                .stream()
                 .map((s) -> s.replace("::", "#"))
+                .map(MethodElementName::new)
                 .collect(Collectors.toList());
-        return failedTests;
     }
 
-    public static List<String> getModifiedClasses(String project, int bugId){
-        Properties properties = getD4jProperties(project, bugId);
-        String raw = properties.getProperty("d4j.classes.modified");
-        List<String> modifiedClasses = new ArrayList<>(List.of(raw.split(",")));
-        return modifiedClasses;
-    }
-
-    @Deprecated
     public static Properties getD4jProperties(String project, int bugId){
-        return getD4jProperties(defects4jDir, project, bugId);
-    }
-
-    public static Properties getD4jProperties(File workDir, String project, int bugId){
         Properties properties = new Properties();
-        String propertyFileName = getProjectDir(workDir, project, bugId, true) + "/defects4j.build.properties";
+        String propertyFileName = getProjectDir(project, bugId, true) + "/defects4j.build.properties";
         try {
             properties.load(Files.newBufferedReader(Paths.get(propertyFileName), StandardCharsets.UTF_8));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
         return properties;
-    }
-
-    public static List<String> getModifiedMethod(String project, int bugId) {
-        Set<String> modifiedMethods = new HashSet<>();
-
-        Defects4jUtil.changeTargetVersion(project, bugId);
-        List<String> modifiedClasses = Defects4jUtil.getModifiedClasses(project, bugId);
-        Properties p = Defects4jUtil.getD4jProperties(project, bugId);
-        String targetRootDir = p.getProperty("d4j.dir.src.classes");
-
-        for(String modifiedClass : modifiedClasses) {
-            String targetSrc = targetRootDir + "/" + modifiedClass.replace(".", "/") + ".java";
-            String[] cmd = new String[]
-                    {"src/main/java/experiment/defect4j/modified_method.sh",
-                            project, String.valueOf(bugId), targetSrc};
-
-            Process proc;
-            try {
-                proc = Runtime.getRuntime().exec(cmd);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-
-            List<String> lines = new ArrayList<>();
-            try (var buf = new BufferedReader(new InputStreamReader(proc.getInputStream()))) {
-                String tmp;
-                while ((tmp = buf.readLine()) != null) {
-                    lines.add(tmp);
-                }
-                proc.waitFor();
-            } catch (InterruptedException | IOException e) {
-                throw new RuntimeException(e);
-            }
-
-            List<Integer> modifiedLines = new ArrayList<>();
-            for (String l : lines) {
-                int modifiedLine = Integer.parseInt(l.split("[+,]")[0]
-                        .replaceAll("[^0-9]", ""));
-                modifiedLines.add(modifiedLine);
-            }
-
-            for(int modifiedLine : modifiedLines){
-                try {
-                    String m = StaticAnalyzer.getMethodNameFormLine(modifiedClass, modifiedLine);
-                    if(m != null) modifiedMethods.add(m);
-                } catch (NoSuchFileException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        }
-
-        return new ArrayList<>(modifiedMethods);
     }
 
     private static void execCmd(String cmd){
