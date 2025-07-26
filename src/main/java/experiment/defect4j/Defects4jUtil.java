b0942fd41dad3322d6c2c0e5566ec228f4fc11c5
atme627 <ezaki.m.aa@m.titech.ac.jp>
2025-07-26 15:57:43 +0900

    feat: add BuggyElementExtractor for extracting buggy lines based on git commits and integrate it with Defects4jUtil

--- src/main/java/experiment/defect4j/Defects4jUtil.java
+++ src/main/java/experiment/defect4j/Defects4jUtil.java
@@ -1,7 +1,14 @@
 package experiment.defect4j;
 
 import jisd.fl.util.PropertyLoader;
+import jisd.fl.util.analyze.LineElementName;
 import jisd.fl.util.analyze.StaticAnalyzer;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -16,17 +23,23 @@ import java.util.stream.Collectors;
 
 public class Defects4jUtil {
     static File defects4jDir = new File("/Users/ezaki/Desktop/tools/defects4j");
-    static String rootDir = "/Users/ezaki/Desktop/research/experiment/defects4j";
 
-    public static void CheckoutAll(String project, int numberOfBugs){
+    public static void CheckoutAll(String project, int numberOfBugs, File workDir){
         for(int i = 1; i <= numberOfBugs; i++){
-            checkoutBuggySrc(project, i);
+            checkoutBuggySrc(project, i, workDir);
         }
     }
 
+    @Deprecated
     public static void checkoutBuggySrc(String project, int bugId){
         String cmd = "defects4j checkout -p " + project + " -v " + bugId + "b " +
                 "-w " + getProjectDir(project, bugId, true);
+        execCmd(cmd);
+    }
+
+    public static void checkoutBuggySrc(String project, int bugId, File workDir){
+        String cmd = "defects4j checkout -p " + project + " -v " + bugId + "b " +
+                "-w " + workDir.getAbsolutePath() + "/" + project + "_" + bugId + "_buggy";
         execCmd(cmd);
     }
 
@@ -35,20 +48,26 @@ public class Defects4jUtil {
         execCmd(cmd);
     }
 
-
+    @Deprecated
     public static String getProjectDir(String project, int bugId, boolean buggy){
-        return rootDir + "/" + project + "/" + project + "_" + bugId + (buggy ? "_buggy" : "_fixed");
+        return defects4jDir + "/" + project + "/" + project + "_" + bugId + (buggy ? "_buggy" : "_fixed");
     }
 
+    public static File getProjectDir(File rootDir, String project, int bugId, boolean buggy){
+        return new File(rootDir + "/" + project + "/" + project + "_" + bugId + (buggy ? "_buggy" : "_fixed"));
+    }
 
-
+    @Deprecated
     public static void changeTargetVersion(String project, int bugId){
-        Properties p = getD4jProperties(project, bugId);
+        changeTargetVersion(defects4jDir, project, bugId);
+    }
+    public static void changeTargetVersion(File rootDir, String project, int bugId){
+        Properties p = getD4jProperties(rootDir, project, bugId);
 
-        String targetSrcDir = getProjectDir(project, bugId, true) + "/" + p.getProperty("d4j.dir.src.classes");
-        String testSrcDir = getProjectDir(project, bugId, true) + "/" + p.getProperty("d4j.dir.src.tests");
-        String targetBinDir = getProjectDir(project, bugId, true) + "/target/classes";
-        String testBinDir = getProjectDir(project, bugId, true) + "/target/test-classes";
+        String targetSrcDir = getProjectDir(rootDir, project, bugId, true) + "/" + p.getProperty("d4j.dir.src.classes");
+        String testSrcDir = getProjectDir(rootDir, project, bugId, true) + "/" + p.getProperty("d4j.dir.src.tests");
+        String targetBinDir = getProjectDir(rootDir, project, bugId, true) + "/target/classes";
+        String testBinDir = getProjectDir(rootDir, project, bugId, true) + "/target/test-classes";
 
         PropertyLoader.setProperty("targetSrcDir", targetSrcDir);
         PropertyLoader.setProperty("testSrcDir", testSrcDir);
@@ -74,9 +93,14 @@ public class Defects4jUtil {
         return modifiedClasses;
     }
 
+    @Deprecated
     public static Properties getD4jProperties(String project, int bugId){
+        return getD4jProperties(defects4jDir, project, bugId);
+    }
+
+    public static Properties getD4jProperties(File workDir, String project, int bugId){
         Properties properties = new Properties();
-        String propertyFileName = getProjectDir(project, bugId, true) + "/defects4j.build.properties";
+        String propertyFileName = getProjectDir(workDir, project, bugId, true) + "/defects4j.build.properties";
         try {
             properties.load(Files.newBufferedReader(Paths.get(propertyFileName), StandardCharsets.UTF_8));
         } catch (IOException e) {
@@ -144,5 +168,44 @@ public class Defects4jUtil {
         } catch (IOException | InterruptedException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public static List<LineElementName> extractBuggyLines(File projectDir){
+        Repository repository;
+        try {
+            repository = new FileRepositoryBuilder()
+                    .setGitDir(new File(projectDir, ".git"))
+                    .build();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        BuggyElementExtractor extractor = new BuggyElementExtractor(repository);
+        return extractor.buggyLines(getBuggyVersionId(repository), getFixedVersionId(repository));
+    }
+
+    public static ObjectId getBuggyVersionId(Repository repository){
+        return findTagsContaining(repository, "BUGGY_VERSION");
+    }
+
+    public static ObjectId getFixedVersionId(Repository repository){
+        return findTagsContaining(repository, "FIXED_VERSION");
+    }
+
+    private static ObjectId findTagsContaining(Repository repository, String keyword) {
+        try (Git git = new Git(repository)) {
+            try {
+                for (Ref ref : git.tagList().call()) {
+                    String full = ref.getName();                  // 例: refs/tags/D4J_Lang_1b
+                    String tag = full.replace("refs/tags/", "");
+                    if (tag.contains(keyword)) {
+                        return ref.getObjectId();
+                    }
+                }
+            } catch (GitAPIException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        throw new RuntimeException("tag that contains \"" + keyword + "\" not found in " + repository.getDirectory().getAbsolutePath());
     }
 }
--- src/test/java/experiment/defect4j/Defects4jUtilTest.java
+++ src/test/java/experiment/defect4j/Defects4jUtilTest.java
@@ -2,13 +2,29 @@ package experiment.defect4j;
 
 import jisd.fl.util.FileUtil;
 import jisd.fl.util.PropertyLoader;
+import jisd.fl.util.analyze.LineElementName;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.Level;
+import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+
 class Defects4jUtilTest {
+    @BeforeEach
+    void stopLog(){
+        Logger root = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+        root.setLevel(Level.OFF);
+    }
+
 
     @Test
     void changeTargetVersionTest() {
@@ -63,5 +79,59 @@ class Defects4jUtilTest {
             System.out.println(m);
         }
         System.out.println();
+    }
+
+    @Nested
+    class extractBuggyLines {
+        File d4jDir;
+        @BeforeEach
+        void init() {
+            d4jDir = new File("/Users/ezaki/Desktop/research/d4jProjects");
+            Defects4jUtil.changeTargetVersion(d4jDir, "Lang", 1);
+        }
+
+        @Test
+        void lang1() {
+            int bugId = 1;
+            List<LineElementName> result = Defects4jUtil.extractBuggyLines(targetProjectDir(bugId));
+
+            assertThat(result, containsInAnyOrder(
+                    new LineElementName("org.apache.commons.lang3.math.NumberUtils#createNumber(String)", 467),
+                    new LineElementName("org.apache.commons.lang3.math.NumberUtils#createNumber(String)", 468),
+                    new LineElementName("org.apache.commons.lang3.math.NumberUtils#createNumber(String)", 471)
+            ));
+        }
+
+        @Test
+        void lang3() {
+            int bugId = 3;
+            List<LineElementName> result = Defects4jUtil.extractBuggyLines(targetProjectDir(bugId));
+
+            assertThat(result, containsInAnyOrder(
+                    new LineElementName("org.apache.commons.lang3.math.NumberUtils#createNumber(String)", 593),
+                    new LineElementName("org.apache.commons.lang3.math.NumberUtils#createNumber(String)", 597),
+                    new LineElementName("org.apache.commons.lang3.math.NumberUtils#createNumber(String)", 601),
+                    new LineElementName("org.apache.commons.lang3.math.NumberUtils#createNumber(String)", 605)
+            ));
+        }
+
+        @Test
+        void lang4() {
+            int bugId = 4;
+            List<LineElementName> result = Defects4jUtil.extractBuggyLines(targetProjectDir(bugId));
+
+            assertThat(result, containsInAnyOrder(
+                    new LineElementName("org.apache.commons.lang3.text.translate.LookupTranslator#<ulinit>()", 31),
+                    new LineElementName("org.apache.commons.lang3.text.translate.LookupTranslator#LookupTranslator(CharSequence[][])", 46),
+                    new LineElementName("org.apache.commons.lang3.text.translate.LookupTranslator#LookupTranslator(CharSequence[][])", 51),
+                    new LineElementName("org.apache.commons.lang3.text.translate.LookupTranslator#translate(CharSequence, int, Writer)", 77)
+            ));
+        }
+
+
+        private File targetProjectDir(int bugId) {
+            return Defects4jUtil.getProjectDir(d4jDir, "Lang", bugId, true);
+        }
+
     }
 }
