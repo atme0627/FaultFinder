8237ae5f589f795009f0eed7a37b4735246dd928
atme627 <ezaki.m.aa@m.titech.ac.jp>
2025-07-26 19:30:56 +0900

    refactor: remove unused methods and redundant imports in Defects4jUtilTest for better code maintainability

--- src/test/java/experiment/defect4j/Defects4jUtilTest.java
+++ src/test/java/experiment/defect4j/Defects4jUtilTest.java
@@ -1,6 +1,5 @@
 package experiment.defect4j;
 
-import jisd.fl.util.FileUtil;
 import jisd.fl.util.PropertyLoader;
 import jisd.fl.util.analyze.LineElementName;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,8 +10,6 @@ import ch.qos.logback.classic.Level;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.*;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -33,61 +30,13 @@ class Defects4jUtilTest {
         System.out.println(PropertyLoader.getProperty("junitConsoleLauncherPath"));
     }
 
-    @Test
-    void getAllModifiedMethodTest() throws InterruptedException {
-        String project = "Math";
-        int maxid = 106;
-        for(int bugId = 1; bugId <=  maxid; bugId++){
-            String d = "src/main/resources/dataset/Math/" + project + bugId + "_buggy/";
-            FileUtil.createDirectory(d);
-            FileUtil.createFile(d, "modified_methods.txt");
-            System.out.println("[PROJECT] " + project + "   [ID] " + bugId);
-            List<String> modified = Defects4jUtil.getModifiedMethod(project, bugId);
-
-
-
-            try (FileWriter fw = new FileWriter(d + "/modified_methods.txt")){
-                //メソッドが執せされていない場合(fieldの変更など)
-                if(modified.isEmpty()){
-                    String s = "Not modified.";
-                    System.out.println(s);
-                    fw.write(s);
-                    fw.write("\n");
-                }
-                else {
-                    for (String m : modified) {
-                        System.out.println(m);
-                        fw.write(m);
-                        fw.write("\n");
-                    }
-                }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            System.out.println();
-            Thread.sleep(1000);
-        }
-    }
-
-    @Test
-    void getModifiedMethodTest(){
-        String project = "Math";
-        int bugId = 87;
-        System.out.println("[PROJECT] " + project + "   [ID] " + bugId);
-        List<String> modified = Defects4jUtil.getModifiedMethod(project, bugId);
-        for(String m : modified){
-            System.out.println(m);
-        }
-        System.out.println();
-    }
-
     @Nested
     class extractBuggyLines {
         File d4jDir;
         @BeforeEach
         void init() {
             d4jDir = new File("/Users/ezaki/Desktop/research/d4jProjects");
-            Defects4jUtil.changeTargetVersion(d4jDir, "Lang", 1);
+            Defects4jUtil.changeTargetVersion("Lang", 1);
         }
 
         @Test
