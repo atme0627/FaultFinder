1cb7890618670849e56296d7308ee7086a824d2f
atme627 <ezaki.m.aa@m.titech.ac.jp>
2025-07-27 20:14:39 +0900

    refactor: enhance `SuspiciousVariableFinder` with improved test case handling, add methods to extract neighbor variables and watch return values

--- src/test/java/experiment/util/SuspiciousVariableFinderTest.java
+++ src/test/java/experiment/util/SuspiciousVariableFinderTest.java
@@ -1,5 +1,6 @@
 package experiment.util;
 
+import experiment.defect4j.Defects4jUtil;
 import jisd.fl.probe.info.SuspiciousVariable;
 import jisd.fl.util.PropertyLoader;
 import jisd.fl.util.TestUtil;
@@ -7,38 +8,73 @@ import jisd.fl.util.analyze.MethodElementName;
 import org.junit.jupiter.api.Test;
 
 import java.nio.file.NoSuchFileException;
+import java.util.ArrayList;
 import java.util.List;
 
 class SuspiciousVariableFinderTest {
 
     @Test
     void find1() throws NoSuchFileException {
-        PropertyLoader.setProperty("targetSrcDir", "src/test/resources/jisd/fl/probe/ProbeExTest/SampleProject/src/main/java");
-        PropertyLoader.setProperty("testSrcDir", "src/test/resources/jisd/fl/probe/ProbeExTest/SampleProject/src/test/java");
-        PropertyLoader.setProperty("testBinDir", "src/test/resources/jisd/fl/probe/ProbeExTest/SampleProject/build/classes/java/main");
-        PropertyLoader.setProperty("targetBinDir", "src/test/resources/jisd/fl/probe/ProbeExTest/SampleProject/build/classes/java/test");
+        PropertyLoader.setTargetSrcDir("/Users/ezaki/IdeaProjects/Project4Test/src/main/java");
+        PropertyLoader.setTestSrcDir("/Users/ezaki/IdeaProjects/Project4Test/src/test/java");
 
-        TestUtil.compileForDebug(new MethodElementName("sample.MethodCallTest"));
+        MethodElementName testMethodName = new MethodElementName("org.sample.coverage.ConditionalTest#testXEqualY()");
+
+        TestUtil.compileForDebug(testMethodName);
         SuspiciousVariableFinder finder
-                = new SuspiciousVariableFinder(new MethodElementName("sample.MethodCallTest#methodCall1()"));
+                = new SuspiciousVariableFinder(testMethodName);
         List<SuspiciousVariable> result = finder.find();
         result.forEach(vi -> System.out.println(vi.toString()));
     }
 
     @Test
     void findD4j1() throws NoSuchFileException {
-        PropertyLoader.setProperty("targetSrcDir", "src/test/resources/d4jProject/Math_2_buggy/src/main/java");
-        PropertyLoader.setProperty("testSrcDir", "src/test/resources/d4jProject/Math_2_buggy/src/test/java");
-        PropertyLoader.setProperty("testBinDir", "src/test/resources/d4jProject/Math_2_buggy/target/test-classes");
-        PropertyLoader.setProperty("targetBinDir", "src/test/resources/d4jProject/Math_2_buggy/target/classes");
-        String testClassName = "org.apache.commons.math3.distribution.HypergeometricDistributionTest";
-        String shortTestMethodName = "testMath1021";
-        String testMethodName = testClassName + "#" + shortTestMethodName + "()";
+        Defects4jUtil.changeTargetVersion("Lang", 1);
+        Defects4jUtil.compileBuggySrc("Lang", 1);
+        MethodElementName testMethodName = new MethodElementName("org.apache.commons.lang3.math.NumberUtilsTest#TestLang747()");
 
-        TestUtil.compileForDebug(new MethodElementName("org.apache.commons.math3.distribution.HypergeometricDistributionTest"));
         SuspiciousVariableFinder finder
-                = new SuspiciousVariableFinder(new MethodElementName(testMethodName));
+                = new SuspiciousVariableFinder(testMethodName);
         List<SuspiciousVariable> result = finder.find();
+        result.forEach(vi -> System.out.println(vi.toString()));
+    }
+
+    @Test
+    void findD4j3() throws NoSuchFileException {
+        Defects4jUtil.changeTargetVersion("Lang", 3);
+        Defects4jUtil.compileBuggySrc("Lang", 3);
+        MethodElementName testMethodName = new MethodElementName("org.apache.commons.lang3.math.NumberUtilsTest#testStringCreateNumberEnsureNoPrecisionLoss()");
+
+        SuspiciousVariableFinder finder
+                = new SuspiciousVariableFinder(testMethodName);
+        List<SuspiciousVariable> result = finder.find();
+        result.forEach(vi -> System.out.println(vi.toString()));
+    }
+
+    @Test
+    void findD4j4() throws NoSuchFileException {
+        Defects4jUtil.changeTargetVersion("Lang", 4);
+        Defects4jUtil.compileBuggySrc("Lang", 4);
+        MethodElementName testMethodName = new MethodElementName("org.apache.commons.lang3.text.translate.LookupTranslatorTest#testLang882()");
+
+        SuspiciousVariableFinder finder
+                = new SuspiciousVariableFinder(testMethodName);
+        List<SuspiciousVariable> result = finder.find();
+        result.forEach(vi -> System.out.println(vi.toString()));
+    }
+
+    @Test
+    void findD4j() throws NoSuchFileException {
+        int bugId = 14;
+        Defects4jUtil.changeTargetVersion("Lang", bugId);
+        Defects4jUtil.compileBuggySrc("Lang", bugId);
+        List<SuspiciousVariable> result = new ArrayList<>();
+        List<MethodElementName> failedMethods = Defects4jUtil.getFailedTestMethods("Lang", bugId);
+        for (MethodElementName me : failedMethods) {
+            SuspiciousVariableFinder finder = new SuspiciousVariableFinder(me);
+            result.addAll(finder.find());
+        }
+
         result.forEach(vi -> System.out.println(vi.toString()));
     }
 }
