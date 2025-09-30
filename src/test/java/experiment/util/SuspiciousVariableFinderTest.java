8e72830167eea0bd98086f53190368a70c82f700
atme627 <ezaki.m.aa@m.titech.ac.jp>
2025-09-30 15:47:20 +0900

    意味が不明瞭なテストの削除

--- src/test/java/experiment/util/SuspiciousVariableFinderTest.java
+++ src/test/java/experiment/util/SuspiciousVariableFinderTest.java
@@ -28,58 +28,7 @@ class SuspiciousVariableFinderTest {
         TestUtil.compileForDebug(testMethodName);
         SuspiciousVariableFinder finder
                 = new SuspiciousVariableFinder(testMethodName);
-        List<SuspiciousVariable> result = finder.find();
-        result.forEach(vi -> System.out.println(vi.toString()));
-    }
-
-    @Test
-    void findD4j1() throws NoSuchFileException {
-        Defects4jUtil.changeTargetVersion("Lang", 1);
-        Defects4jUtil.compileBuggySrc("Lang", 1);
-        MethodElementName testMethodName = new MethodElementName("org.apache.commons.lang3.math.NumberUtilsTest#TestLang747()");
-
-        SuspiciousVariableFinder finder
-                = new SuspiciousVariableFinder(testMethodName);
-        List<SuspiciousVariable> result = finder.find();
-        result.forEach(vi -> System.out.println(vi.toString()));
-    }
-
-    @Test
-    void findD4j3() throws NoSuchFileException {
-        Defects4jUtil.changeTargetVersion("Lang", 3);
-        Defects4jUtil.compileBuggySrc("Lang", 3);
-        MethodElementName testMethodName = new MethodElementName("org.apache.commons.lang3.math.NumberUtilsTest#testStringCreateNumberEnsureNoPrecisionLoss()");
-
-        SuspiciousVariableFinder finder
-                = new SuspiciousVariableFinder(testMethodName);
-        List<SuspiciousVariable> result = finder.find();
-        result.forEach(vi -> System.out.println(vi.toString()));
-    }
-
-    @Test
-    void findD4j4() throws NoSuchFileException {
-        Defects4jUtil.changeTargetVersion("Lang", 4);
-        Defects4jUtil.compileBuggySrc("Lang", 4);
-        MethodElementName testMethodName = new MethodElementName("org.apache.commons.lang3.text.translate.LookupTranslatorTest#testLang882()");
-
-        SuspiciousVariableFinder finder
-                = new SuspiciousVariableFinder(testMethodName);
-        List<SuspiciousVariable> result = finder.find();
-        result.forEach(vi -> System.out.println(vi.toString()));
-    }
-
-    @Test
-    void findD4j() throws NoSuchFileException {
-        int bugId = 14;
-        Defects4jUtil.changeTargetVersion("Lang", bugId);
-        Defects4jUtil.compileBuggySrc("Lang", bugId);
-        List<SuspiciousVariable> result = new ArrayList<>();
-        List<MethodElementName> failedMethods = Defects4jUtil.getFailedTestMethods("Lang", bugId);
-        for (MethodElementName me : failedMethods) {
-            SuspiciousVariableFinder finder = new SuspiciousVariableFinder(me);
-            result.addAll(finder.find());
-        }
-
+        List<SuspiciousVariable> result = finder.findSuspiciousVariableInAssertLine();
         result.forEach(vi -> System.out.println(vi.toString()));
     }
 }
