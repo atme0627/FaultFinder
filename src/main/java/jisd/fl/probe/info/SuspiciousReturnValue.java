55ddaa702980204f69546c3ceaf6db3b376fb0a9
atme627 <ezaki.m.aa@m.titech.ac.jp>
2025-10-01 12:35:08 +0900

    minor changes

--- src/main/java/jisd/fl/probe/info/SuspiciousReturnValue.java
+++ src/main/java/jisd/fl/probe/info/SuspiciousReturnValue.java
@@ -54,7 +54,7 @@ public class SuspiciousReturnValue extends SuspiciousExpression {
 
         //Debugger生成
         String main = TestUtil.getJVMMain(this.failedTest);
-        String options = TestUtil.getJVMOptionWithGetDebugBinDir();
+        String options = TestUtil.getJVMOption();
         EnhancedDebugger eDbg = new EnhancedDebugger(main, options);
         //ブレークポイントにヒットした時に行う処理を定義
         //ここではその行で呼ばれてるメソッド情報を抽出
