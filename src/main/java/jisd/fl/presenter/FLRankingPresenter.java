b5006b04d19609b80edc9347440b9d2ab8fe6005
atme627 <ezaki.m.aa@m.titech.ac.jp>
2025-12-25 16:19:38 +0900

    extract FL results printing logic to FLRankingPresenter

---
+++ src/main/java/jisd/fl/presenter/FLRankingPresenter.java
@@ -0,0 +1,85 @@
+package jisd.fl.presenter;
+
+import jisd.fl.ranking.FLRanking;
+import jisd.fl.ranking.FLRankingElement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.Math.min;
+
+public class FLRankingPresenter {
+    private final FLRanking ranking;
+    public FLRankingPresenter(FLRanking ranking){
+        this.ranking = ranking;
+    }
+
+    public void addClassFilter(String pattern){
+
+    }
+    public void print(){
+
+    }
+
+    public void printFLResults() {
+        printFLResults(ranking.getSize());
+    }
+
+    public void printFLResults(int top){
+        ranking.sort();
+        List<String> shortClassNames = new ArrayList<>();
+        List<String> shortMethodNames = new ArrayList<>();
+        for(int i = 0; i < min(top, ranking.getSize()); i++){
+            shortClassNames.add(ranking.at(i).getCodeElementName().compressedClassName());
+            shortMethodNames.add(ranking.at(i).getCodeElementName().compressedShortMethodName());
+        }
+
+        int classLength = shortClassNames.stream().map(String::length).max(Integer::compareTo).get();
+        int methodLength = shortMethodNames.stream().map(String::length).max(Integer::compareTo).get();
+
+        String header = "|      | RANK |" + leftPad(" CLASS NAME ", classLength) +
+                "|" + rightPad(" METHOD NAME ", methodLength +2 )  +
+                "| SUSP SCORE |";
+        String partition = "=".repeat(header.length());
+
+        System.out.println("[  SBFL RANKING  ]");
+        System.out.println(partition);
+        System.out.println(header);
+        System.out.println(partition);
+        int previousRank = 1;
+        for(int i = 0; i < min(top, ranking.getSize()); i++){
+            FLRankingElement element = ranking.at(i);
+            //同率を考慮する
+            int rank = 0;
+            if(i == 0) {
+                rank = i+1;
+            }
+            else {
+                if(element.isSameScore(ranking.at(i-1))){
+                    rank = previousRank;
+                }
+                else {
+                    rank = i+1;
+                }
+            }
+
+            String colorBegin = "";
+            String coloerEnd = "";
+            System.out.println(colorBegin + "| " + String.format("%3d ", i + 1) + " | " + String.format("%3d ", rank) + " | " +
+                    leftPad(shortClassNames.get(i), classLength) + " | " +
+                    rightPad(shortMethodNames.get(i), methodLength) + " | " +
+                    String.format("  %.4f  ", element.getSuspiciousnessScore()) + " |" + coloerEnd);
+            previousRank = rank;
+        }
+        System.out.println(partition);
+        System.out.println();
+    }
+
+    public static String leftPad(String str, int size){
+        return ("%-" + size + "s").formatted(str);
+    }
+
+    public static String rightPad(String str, int size){
+        return ("%" + size + "s").formatted(str);
+    }
+}
--- src/main/java/jisd/fl/ranking/FLRanking.java
+++ src/main/java/jisd/fl/ranking/FLRanking.java
@@ -32,64 +32,6 @@ public class FLRanking {
         return ranking.size();
     }
 
-    public void printFLResults() {
-        printFLResults(getSize());
-    }
-
-    public void printFLResults(int top){
-        sort();
-        List<String> shortClassNames = new ArrayList<>();
-        List<String> shortMethodNames = new ArrayList<>();
-        for(int i = 0; i < min(top, getSize()); i++){
-            shortClassNames.add(ranking.get(i).getCodeElementName().compressedClassName());
-            shortMethodNames.add(ranking.get(i).getCodeElementName().compressedShortMethodName());
-        }
-
-        int classLength = shortClassNames.stream().map(String::length).max(Integer::compareTo).get();
-        int methodLength = shortMethodNames.stream().map(String::length).max(Integer::compareTo).get();
-
-        String header = "|      | RANK |" + leftPad(" CLASS NAME ", classLength) +
-                "|" + rightPad(" METHOD NAME ", methodLength +2 )  +
-                "| SUSP SCORE |";
-        String partition = "=".repeat(header.length());
-
-        System.out.println("[  SBFL RANKING  ]");
-        System.out.println(partition);
-        System.out.println(header);
-        System.out.println(partition);
-        int previousRank = 1;
-        for(int i = 0; i < min(top, getSize()); i++){
-            FLRankingElement element = ranking.get(i);
-            //同率を考慮する
-            int rank = 0;
-            if(i == 0) {
-                rank = i+1;
-            }
-            else {
-                if(element.isSameScore(ranking.get(i-1))){
-                    rank = previousRank;
-                }
-                else {
-                    rank = i+1;
-                }
-            }
-
-            String colorBegin = "";
-            String coloerEnd = "";
-            if(highlightMethods.contains(element.toString())){
-                colorBegin = "\u001b[00;41m";
-                coloerEnd = "\u001b[00m";
-            }
-            System.out.println(colorBegin + "| " + String.format("%3d ", i + 1) + " | " + String.format("%3d ", rank) + " | " +
-                    leftPad(shortClassNames.get(i), classLength) + " | " +
-                    rightPad(shortMethodNames.get(i), methodLength) + " | " +
-                    String.format("  %.4f  ", element.getSuspiciousnessScore()) + " |" + coloerEnd);
-            previousRank = rank;
-        }
-        System.out.println(partition);
-        System.out.println();
-    }
-
     public Optional<FLRankingElement> getElementAtPlace(int place){
         return place <= ranking.size() ? Optional.of(ranking.get(place - 1)) : Optional.empty();
     }
