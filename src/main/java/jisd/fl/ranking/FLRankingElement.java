4a248fde6150605a6dabe5efe142d54044c7e821
atme627 <ezaki.m.aa@m.titech.ac.jp>
2026-01-05 12:57:07 +0900

    rename: e -> element

--- src/main/java/jisd/fl/ranking/FLRankingElement.java
+++ src/main/java/jisd/fl/ranking/FLRankingElement.java
@@ -5,17 +5,17 @@ import jisd.fl.util.analyze.CodeElementName;
 import java.util.Objects;
 
 public class FLRankingElement implements Comparable<FLRankingElement> {
-    public final CodeElementName e;
+    public final CodeElementName element;
     public double suspScore;
 
     FLRankingElement(CodeElementName e, double suspScore) {
-        this.e = e;
+        this.element = e;
         this.suspScore = suspScore;
     }
 
     @Override
     public int compareTo(FLRankingElement o) {
-        return isSameScore(o) ? e.compareTo(o.e) : -Double.compare(this.suspScore, o.suspScore);
+        return isSameScore(o) ? element.compareTo(o.element) : -Double.compare(this.suspScore, o.suspScore);
     }
 
     //小数点以下4桁までで比較
@@ -24,7 +24,7 @@ public class FLRankingElement implements Comparable<FLRankingElement> {
     }
 
     public CodeElementName getCodeElementName() {
-        return e;
+        return element;
     }
 
     public double getSuspiciousnessScore() {
@@ -36,19 +36,19 @@ public class FLRankingElement implements Comparable<FLRankingElement> {
         if (obj == this) return true;
         if (obj == null || obj.getClass() != this.getClass()) return false;
         var that = (FLRankingElement) obj;
-        return Objects.equals(this.e, that.e) &&
+        return Objects.equals(this.element, that.element) &&
                 Double.doubleToLongBits(this.suspScore) == Double.doubleToLongBits(that.suspScore);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(e, suspScore);
+        return Objects.hash(element, suspScore);
     }
 
     @Override
     public String toString() {
         return "FLRankingElement[" +
-                "e=" + e + ", " +
+                "e=" + element + ", " +
                 "sbflScore=" + suspScore + ']';
     }
 
