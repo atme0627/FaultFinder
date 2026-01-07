55ebfd13c06a3c80ebe56500ea6f1466fca1e9c8
atme627 <ezaki.m.aa@m.titech.ac.jp>
2026-01-07 13:34:33 +0900

    未使用のgetCallCountBeforeTargetArgEvalメソッドを削除

--- src/main/java/jisd/fl/probe/info/TmpJavaParserUtils.java
+++ src/main/java/jisd/fl/probe/info/TmpJavaParserUtils.java
@@ -12,22 +12,6 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 public class TmpJavaParserUtils {
-    //対象の引数の演算の前に何回メソッド呼び出しが行われるかを計算する。
-    //まず、stmtでのメソッド呼び出しをJava の実行時評価順でソートしたリストを取得
-    //はじめのノードから順に探し、親にexprを持つものがあったら、その時のindexが求めたい値
-    static int getCallCountBeforeTargetArgEval(Statement stmt, Expression targetExpr){
-        List<Expression> calls = new ArrayList<>();
-        Expression targetExpr = extractExpr(false);
-        stmt.accept(new SuspiciousArgument.EvalOrderVisitor(), calls);
-        for(Expression call : calls){
-            if(call == targetExpr || call.findAncestor(Node.class, anc -> anc == targetExpr).isPresent()){
-                return calls.indexOf(call) + 1;
-            }
-        }
-        throw new RuntimeException("Something is wrong.");
-    }
-
-
     static public Statement extractStmt(MethodElementName locateMethod, int locateLine) {
         try {
             return JavaParserUtil.getStatementByLine(locateMethod, locateLine).orElseThrow();
