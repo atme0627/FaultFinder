af05e040fd13f2429d4c1625b8ac143f84297aa4
atme627 <ezaki.m.aa@m.titech.ac.jp>
2026-01-07 16:03:22 +0900

    SuspiciousVariableFinderにTODOコメントを追加し、コードの改善箇所を示す

--- src/main/java/experiment/util/SuspiciousVariableFinder.java
+++ src/main/java/experiment/util/SuspiciousVariableFinder.java
@@ -62,10 +62,12 @@ public class SuspiciousVariableFinder {
                 .collect(Collectors.toList());
 
         //失敗行で呼び出しが行われているメソッドの情報を動的解析で取得し、それらのreturn行で使用されている変数の情報を抽出する。
+        //TODO: treeNodeをなんとかする
         List<SuspiciousExpression> returns = methodCallWatcher.searchSuspiciousReturns(failureLine, locateMethod);
         for (SuspiciousExpression r : returns) {
             List<SuspiciousVariable> neighbor = r.neighborSuspiciousVariables(2000, false, r);
             result.addAll(neighbor);
+
         }
 
         return result;
