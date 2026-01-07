e84e4e2b5209e28b0760645e1568eede4441fe9b
atme627 <ezaki.m.aa@m.titech.ac.jp>
2026-01-07 15:30:03 +0900

    TmpJsonUtilsクラスのloadFromJsonメソッドの実装を一時的に削除して未実装状態に変更

--- src/main/java/jisd/fl/probe/info/TmpJsonUtils.java
+++ src/main/java/jisd/fl/probe/info/TmpJsonUtils.java
@@ -6,13 +6,8 @@ import java.io.File;
 
 public class TmpJsonUtils {
     //SuspiciousExpressionリファクタリングのための一時的なクラス
-    //Jackson デシリアライズ用メソッド
     public static SuspiciousExpression loadFromJson(File f){
-        ObjectMapper mapper = new ObjectMapper();
-        try {
-            return mapper.readValue(f, SuspiciousExpression.class);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        //TODO: implement
+        return null;
     }
 }
