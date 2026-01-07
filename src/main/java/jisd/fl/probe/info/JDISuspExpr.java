57d56baf8a7ec306a8ad13e62478f83819b97a33
atme627 <ezaki.m.aa@m.titech.ac.jp>
2026-01-07 15:20:10 +0900

    neighborSuspiciousVariablesメソッドをJDISuspExprクラスへ移動し、コードの責任範囲を明確化

--- src/main/java/jisd/fl/probe/info/JDISuspExpr.java
+++ src/main/java/jisd/fl/probe/info/JDISuspExpr.java
@@ -1,6 +1,10 @@
 package jisd.fl.probe.info;
 
+import jisd.fl.core.entity.susp.SuspiciousVariable;
 import jisd.fl.probe.record.TracedValueCollection;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class JDISuspExpr {
     /**
@@ -18,5 +22,32 @@ public class JDISuspExpr {
             case SuspiciousArgument thisSuspArg -> JDISuspArg.traceAllValuesAtSuspExpr(sleepTime, thisSuspArg);
             default -> throw new IllegalStateException("Unexpected value: " + thisSuspExpr);
         };
+    }
+
+    static public List<SuspiciousVariable> neighborSuspiciousVariables(int sleepTime, boolean includeIndirectUsedVariable, SuspiciousExpression suspExpr){
+        //SuspExprで観測できる全ての変数
+        TracedValueCollection tracedNeighborValue = JDISuspExpr.traceAllValuesAtSuspExpr(sleepTime, suspExpr);
+        //SuspExpr内で使用されている変数を静的解析により取得
+        List<String> neighborVariableNames = TmpJavaParserUtils.extractNeighborVariableNames(suspExpr.expr, includeIndirectUsedVariable || suspExpr.childSuspExprs.isEmpty());
+
+        //TODO: 今の実装だと配列のフィルタリングがうまくいかない
+        //TODO: 今の実装だと、変数がローカルかフィールドか区別できない
+        // ex. this.x = x の時, this.xも探索してしまう。
+        List<SuspiciousVariable> result =
+                tracedNeighborValue.getAll().stream()
+                        .filter(t -> neighborVariableNames.contains(t.variableName))
+                        .filter(t -> !t.isReference)
+                        //
+                        .map(t -> new SuspiciousVariable(
+                                suspExpr.failedTest,
+                                suspExpr.locateMethod.getFullyQualifiedMethodName(),
+                                t.variableName,
+                                t.value,
+                                true,
+                                t.isField
+                        )).distinct().collect(Collectors.toList());
+
+        result.forEach(sv -> sv.setParent(suspExpr));
+        return result;
     }
 }
--- src/main/java/jisd/fl/probe/info/SuspiciousExpression.java
+++ src/main/java/jisd/fl/probe/info/SuspiciousExpression.java
@@ -104,31 +104,9 @@ public abstract class SuspiciousExpression {
      * @param suspExpr
      * @return
      */
-    public List<SuspiciousVariable> neighborSuspiciousVariables(int sleepTime, boolean includeIndirectUsedVariable, SuspiciousExpression suspExpr){
+    static public List<SuspiciousVariable> neighborSuspiciousVariables(int sleepTime, boolean includeIndirectUsedVariable, SuspiciousExpression suspExpr){
         //SuspExprで観測できる全ての変数
-        TracedValueCollection tracedNeighborValue = JDISuspExpr.traceAllValuesAtSuspExpr(sleepTime, suspExpr);
-        //SuspExpr内で使用されている変数を静的解析により取得
-        List<String> neighborVariableNames = TmpJavaParserUtils.extractNeighborVariableNames(suspExpr.expr, includeIndirectUsedVariable || suspExpr.childSuspExprs.isEmpty());
-
-        //TODO: 今の実装だと配列のフィルタリングがうまくいかない
-        //TODO: 今の実装だと、変数がローカルかフィールドか区別できない
-        // ex. this.x = x の時, this.xも探索してしまう。
-        List<SuspiciousVariable> result =
-                tracedNeighborValue.getAll().stream()
-                .filter(t -> neighborVariableNames.contains(t.variableName))
-                .filter(t -> !t.isReference)
-                        //
-                .map(t -> new SuspiciousVariable(
-                        suspExpr.failedTest,
-                        suspExpr.locateMethod.getFullyQualifiedMethodName(),
-                        t.variableName,
-                        t.value,
-                        true,
-                        t.isField
-                )).distinct().collect(Collectors.toList());
-
-        result.forEach(sv -> sv.setParent(this));
-        return result;
+        return JDISuspExpr.neighborSuspiciousVariables(sleepTime, includeIndirectUsedVariable, suspExpr);
     }
 
     public void addChild(SuspiciousExpression ch){
