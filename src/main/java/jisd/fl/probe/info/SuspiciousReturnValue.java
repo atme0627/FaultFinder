ea246a933bd8c2229964867cfcbe712bd75fc716
atme627 <ezaki.m.aa@m.titech.ac.jp>
2026-01-07 15:32:39 +0900

    SuspiciousReturnValueクラスからJacksonアノテーションとシリアライズ関連メソッドを削除し、不要なコードを整理

--- src/main/java/jisd/fl/probe/info/SuspiciousReturnValue.java
+++ src/main/java/jisd/fl/probe/info/SuspiciousReturnValue.java
@@ -1,32 +1,13 @@
 package jisd.fl.probe.info;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import jisd.fl.core.entity.MethodElementName;
 
 import java.util.List;
 import java.util.NoSuchElementException;
 
-@JsonIgnoreProperties(ignoreUnknown = true)
-@JsonPropertyOrder({ "failedTest", "locateMethod", "locateLine", "stmt", "expr", "actualValue", "children" })
-
 public class SuspiciousReturnValue extends SuspiciousExpression {
     public SuspiciousReturnValue(MethodElementName failedTest, MethodElementName locateMethod, int locateLine, String actualValue) {
         super(failedTest, locateMethod, locateLine, actualValue);
-        this.expr = JavaParserSuspReturn.extractExprReturnValue(stmt);
-    }
-
-    @JsonCreator
-    private SuspiciousReturnValue(
-            @JsonProperty("failedTest") String failedTest,
-            @JsonProperty("locateMethod") String locateMethod,
-            @JsonProperty("locateLine") int locateLine,
-            @JsonProperty("actualValue") String actualValue,
-            @JsonProperty("children") List<SuspiciousExpression> children
-    ){
-        super(failedTest, locateMethod, locateLine, actualValue, children);
         this.expr = JavaParserSuspReturn.extractExprReturnValue(stmt);
     }
     
