b2c956d86c35f891d60ad5e3d2c60ea1ec69d0c8
atme627 <ezaki.m.aa@m.titech.ac.jp>
2026-01-07 14:39:15 +0900

    未使用のimport文とvalidateIsTargetExecutionメソッドを削除

--- src/main/java/jisd/fl/probe/info/SuspiciousReturnValue.java
+++ src/main/java/jisd/fl/probe/info/SuspiciousReturnValue.java
@@ -4,24 +4,9 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import com.sun.jdi.IncompatibleThreadStateException;
-import com.sun.jdi.StackFrame;
-import com.sun.jdi.ThreadReference;
-import com.sun.jdi.event.Event;
-import com.sun.jdi.event.EventSet;
-import com.sun.jdi.event.MethodExitEvent;
-import com.sun.jdi.event.StepEvent;
-import com.sun.jdi.request.EventRequestManager;
-import com.sun.jdi.request.MethodExitRequest;
-import com.sun.jdi.request.StepRequest;
-import jisd.debug.EnhancedDebugger;
-import jisd.fl.probe.record.TracedValue;
 import jisd.fl.probe.record.TracedValueCollection;
-import jisd.fl.probe.record.TracedValuesAtLine;
-import jisd.fl.util.TestUtil;
 import jisd.fl.core.entity.MethodElementName;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 
@@ -49,10 +34,6 @@ public class SuspiciousReturnValue extends SuspiciousExpression {
     @Override
     public List<SuspiciousReturnValue> searchSuspiciousReturns() throws NoSuchElementException {
         return JDISuspReturn.searchSuspiciousReturns(this);
-    }
-
-    static private boolean validateIsTargetExecution(MethodExitEvent recent, String actualValue){
-        return TmpJDIUtils.getValueString(recent.returnValue()).equals(actualValue);
     }
 
 
