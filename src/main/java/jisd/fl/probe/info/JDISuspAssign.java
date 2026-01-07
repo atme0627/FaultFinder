f7f8ac0f45bb0dc52a96b59ef1feedeec11b0a35
atme627 <ezaki.m.aa@m.titech.ac.jp>
2026-01-07 14:28:59 +0900

    searchSuspiciousReturnsメソッドをJDISuspAssignクラスへ移動し、SuspiciousAssignmentクラスからの依存を削減

--- src/main/java/jisd/fl/probe/info/JDISuspAssign.java
+++ src/main/java/jisd/fl/probe/info/JDISuspAssign.java
@@ -3,10 +3,13 @@ package jisd.fl.probe.info;
 import com.sun.jdi.*;
 import com.sun.jdi.event.Event;
 import com.sun.jdi.event.EventSet;
+import com.sun.jdi.event.MethodExitEvent;
 import com.sun.jdi.event.StepEvent;
 import com.sun.jdi.request.EventRequestManager;
+import com.sun.jdi.request.MethodExitRequest;
 import com.sun.jdi.request.StepRequest;
 import jisd.debug.EnhancedDebugger;
+import jisd.fl.core.entity.MethodElementName;
 import jisd.fl.core.entity.susp.SuspiciousVariable;
 import jisd.fl.probe.record.TracedValue;
 import jisd.fl.probe.record.TracedValueCollection;
@@ -18,6 +21,92 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 public class JDISuspAssign {
+    //TODO: 今はオブジェクトの違いを考慮していない
+    static List<SuspiciousReturnValue> searchSuspiciousReturns(SuspiciousAssignment thisSuspAssign) throws NoSuchElementException {
+        final List<SuspiciousReturnValue> result = new ArrayList<>();
+        if(!TmpJavaParserUtils.hasMethodCalling(thisSuspAssign.expr)) return result;
+
+        //Debugger生成
+        String main = TestUtil.getJVMMain(thisSuspAssign.failedTest);
+        String options = TestUtil.getJVMOption();
+        EnhancedDebugger eDbg = new EnhancedDebugger(main, options);
+
+        //対象の引数が属する行にたどり着いた時に行う処理を定義
+        //ここではその行で呼ばれてるメソッド情報を抽出
+        EnhancedDebugger.BreakpointHandler handler = (vm, bpe) -> {
+            //既に情報が取得できている場合は終了
+            if(!result.isEmpty()) return;
+
+            List<SuspiciousReturnValue> resultCandidate = new ArrayList<>();
+            EventRequestManager manager = vm.eventRequestManager();
+
+            //このスレッドでの MethodExit を記録するリクエストを作成
+            ThreadReference thread = bpe.thread();
+            MethodExitRequest meReq = EnhancedDebugger.createMethodExitRequest(manager, thread);
+            //この行の実行が終わったことを検知するステップリクエストを作成
+            //この具象クラスではステップイベントの通知タイミングで、今調査していた行が調べたい行だったかを確認
+            StepRequest stepReq = EnhancedDebugger.createStepOverRequest(manager, thread);
+
+            // ブレークポイント地点でのコールスタックの深さを取得
+            // 呼び出しメソッドの取得条件を 深さ == depthBeforeCall + 1　にすることで
+            // 再帰呼び出し含め、その行で直接呼ばれたメソッドのみ取ってこれる
+            int depthBeforeCall = TmpJDIUtils.getCallStackDepth(thread);
+
+            //一旦 resume して、内部ループで MethodExit／Step を待つ
+            vm.resume();
+            boolean done = false;
+            while (!done) {
+                EventSet es = vm.eventQueue().remove();
+                for (Event ev : es) {
+                    //実行された、とあるメソッドから抜けた
+                    if (ev instanceof MethodExitEvent) {
+                        MethodExitEvent mee = (MethodExitEvent) ev;
+
+                        //収集するのは指定した行で直接呼び出したメソッドのみ
+                        //depthBeforeCallとコールスタックの深さを比較することで直接呼び出したメソッドかどうかを判定
+                        if (mee.thread().equals(thread) && TmpJDIUtils.getCallStackDepth(mee.thread()) == depthBeforeCall + 1) {
+                            MethodElementName invokedMethod = new MethodElementName(EnhancedDebugger.getFqmn(mee.method()));
+                            int locateLine = mee.location().lineNumber();
+                            String actualValue = TmpJDIUtils.getValueString(mee.returnValue());
+                            try {
+                                SuspiciousReturnValue suspReturn = new SuspiciousReturnValue(
+                                        thisSuspAssign.failedTest,
+                                        invokedMethod,
+                                        locateLine,
+                                        actualValue
+                                );
+                                resultCandidate.add(suspReturn);
+                            } catch (RuntimeException e) {
+                                System.out.println("cannot create SuspiciousReturnValue: " + e.getMessage() + " at " + invokedMethod + " line:" + locateLine);
+                            }
+                        }
+                        vm.resume();
+                    }
+                    //調査対象の行の実行(実行インスタンス)が終了
+                    //ここで、調査した行が目的のものであったかチェック
+                    if (ev instanceof StepEvent) {
+                        done = true;
+                        StepEvent se = (StepEvent) ev;
+                        if(JDISuspAssign.validateIsTargetExecution(se, thisSuspAssign.assignTarget)) result.addAll(resultCandidate);
+                        //vmをresumeしない
+                    }
+                }
+            }
+            //動的に作ったリクエストを無効化
+            meReq.disable();
+            stepReq.disable();
+        };
+
+        //VMを実行し情報を収集
+        eDbg.handleAtBreakPoint(thisSuspAssign.locateMethod.getFullyQualifiedClassName(), thisSuspAssign.locateLine, handler);
+        if(result.isEmpty()){
+            System.err.println("[[searchSuspiciousReturns]] Could not confirm [ "
+                    + "(return value) == " + thisSuspAssign.actualValue
+                    + " ] on " + thisSuspAssign.locateMethod + " line:" + thisSuspAssign.locateLine);
+        }
+        return result;
+    }
+
     static  TracedValueCollection traceAllValuesAtSuspExpr(int sleepTime, SuspiciousAssignment thisSuspExpr){
         final List<TracedValue> result = new ArrayList<>();
 
