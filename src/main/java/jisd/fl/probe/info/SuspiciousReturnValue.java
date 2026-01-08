748348facc6ca913fb5c777018e86ffde04eca6d
atme627 <ezaki.m.aa@m.titech.ac.jp>
2026-01-08 14:26:00 +0900

    SuspiciousReturnValueコンストラクタから不要なオーバーロードを削除し、シンプル化を実施

--- src/main/java/jisd/fl/probe/info/SuspiciousReturnValue.java
+++ src/main/java/jisd/fl/probe/info/SuspiciousReturnValue.java
@@ -7,11 +7,6 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 public class SuspiciousReturnValue extends SuspiciousExpression {
-    public SuspiciousReturnValue(MethodElementName failedTest, MethodElementName locateMethod, int locateLine, String actualValue) {
-        super(failedTest, locateMethod, locateLine, actualValue);
-        Statement stmt = TmpJavaParserUtils.extractStmt(this.locateMethod, this.locateLine);
-        this.expr = JavaParserSuspReturn.extractExprReturnValue(stmt);
-    }
 
     public SuspiciousReturnValue(MethodElementName failedTest, MethodElementName locateMethod, int locateLine, String actualValue, String stmtString) {
         super(failedTest, locateMethod, locateLine, actualValue, stmtString);
