3d833f92d17934291b79c268510a27a0a89d0d8f
atme627 <ezaki.m.aa@m.titech.ac.jp>
2026-01-08 15:00:35 +0900

    exprへの依存解消 - targetMethodNameフィールドを追加し、メソッドを簡素化

--- src/main/java/jisd/fl/probe/info/SuspiciousArgument.java
+++ src/main/java/jisd/fl/probe/info/SuspiciousArgument.java
@@ -22,14 +22,17 @@ public class SuspiciousArgument extends SuspiciousExpression {
     //対象の引数内の最初のmethodCallがstmtで何番目か
     final int targetCallCount;
 
+    //return位置を調べたいmethod一覧
+    final List<String> targetMethodName;
+
     public SuspiciousArgument(MethodElementName failedTest,
-                               MethodElementName locateMethod,
-                               int locateLine,
-                               String actualValue,
-                               MethodElementName calleeMethodName,
-                               int argIndex,
-                               int CallCountAfterTargetInLine,
-                               String stmtString) {
+                              MethodElementName locateMethod,
+                              int locateLine,
+                              String actualValue,
+                              MethodElementName calleeMethodName,
+                              int argIndex,
+                              int CallCountAfterTargetInLine,
+                              String stmtString) {
         super(failedTest, locateMethod, locateLine, actualValue, stmtString);
         this.argIndex = argIndex;
         this.calleeMethodName = calleeMethodName;
@@ -41,6 +44,13 @@ public class SuspiciousArgument extends SuspiciousExpression {
         this.targetCallCount = JavaParserSuspArg.getCallCountBeforeTargetArgEval(stmt, this.CallCountAfterTargetInLine, this.argIndex, this.calleeMethodName);
 
         this.hasMethodCalling = !this.expr.findAll(MethodCallExpr.class).isEmpty();
+
+
+        this.targetMethodName = this.expr.findAll(MethodCallExpr.class)
+                .stream()
+                .filter(mce -> mce.findAncestor(MethodCallExpr.class).isEmpty())
+                .map(mce -> mce.getName().toString())
+                .collect(Collectors.toList());
     }
 
     @Override
@@ -48,7 +58,7 @@ public class SuspiciousArgument extends SuspiciousExpression {
     //引数を区別せず、引数の評価の際に呼ばれたすべてのメソッドについて情報を取得し
     //Expressionを静的解析してexpressionで直接呼ばれてるメソッドのみに絞る
     //ex.) expressionがx.f(y.g())の時、fのみとる。y.g()はfの探索の後行われるはず
-    public List<SuspiciousReturnValue> searchSuspiciousReturns() throws NoSuchElementException{
+    public List<SuspiciousReturnValue> searchSuspiciousReturns() throws NoSuchElementException {
         return JDISuspArg.searchSuspiciousReturns(this);
     }
 
@@ -60,19 +70,19 @@ public class SuspiciousArgument extends SuspiciousExpression {
     /**
      * ある変数がその値を取る原因が呼び出し元の引数のあると判明した場合に使用
      */
-    static public Optional<SuspiciousArgument> searchSuspiciousArgument(MethodElementName calleeMethodName, SuspiciousVariable suspVar){
+    static public Optional<SuspiciousArgument> searchSuspiciousArgument(MethodElementName calleeMethodName, SuspiciousVariable suspVar) {
         return JDISuspArg.searchSuspiciousArgument(calleeMethodName, suspVar);
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         return "[ SUSPICIOUS ARGUMENT ] ( " + locateMethod + " line:" + locateLine + " ) " + stmtString();
     }
 
 
     private static String createStmtString(Statement stmt, int callCountAfterTargetInLine, int argIndex, MethodElementName calleeMethodName) {
         final String BG_GREEN = "\u001B[42m";
-        final String RESET    = "\u001B[0m";
+        final String RESET = "\u001B[0m";
         LexicalPreservingPrinter.setup(stmt);
         extractExprArg(false, stmt, callCountAfterTargetInLine, argIndex, calleeMethodName).getTokenRange().ifPresent(tokenRange -> {
                     // 子ノードに属するすべてのトークンに色付け
@@ -87,16 +97,12 @@ public class SuspiciousArgument extends SuspiciousExpression {
     }
 
     @Override
-    public String stmtString(){
+    public String stmtString() {
         return stmtString;
     }
 
     //引数の静的解析により、return位置を調べたいmethod一覧を取得する
-    List<String> targetMethodName(){
-        return this.expr.findAll(MethodCallExpr.class)
-                .stream()
-                .filter(mce -> mce.findAncestor(MethodCallExpr.class).isEmpty())
-                .map(mce -> mce.getName().toString())
-                .collect(Collectors.toList());
+    List<String> targetMethodName() {
+        return targetMethodName;
     }
 }
