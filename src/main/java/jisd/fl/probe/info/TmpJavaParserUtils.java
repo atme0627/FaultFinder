eac6352b9d1d1e08f9b1a29373ca4daeab597832
atme627 <ezaki.m.aa@m.titech.ac.jp>
2026-01-13 15:51:59 +0900

    不要なメソッド `extractNeighborVariableNames` を削除し、コードを整理

--- src/main/java/jisd/fl/probe/info/TmpJavaParserUtils.java
+++ src/main/java/jisd/fl/probe/info/TmpJavaParserUtils.java
@@ -1,16 +1,11 @@
 package jisd.fl.probe.info;
 
-import com.github.javaparser.ast.expr.Expression;
-import com.github.javaparser.ast.expr.MethodCallExpr;
-import com.github.javaparser.ast.expr.NameExpr;
 import com.github.javaparser.ast.stmt.Statement;
 import jisd.fl.core.entity.MethodElementName;
 import jisd.fl.util.analyze.JavaParserUtil;
 
 import java.nio.file.NoSuchFileException;
-import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.stream.Collectors;
 
 public class TmpJavaParserUtils {
     static public Statement extractStmt(MethodElementName locateMethod, int locateLine) {
@@ -21,18 +16,5 @@ public class TmpJavaParserUtils {
         } catch (NoSuchElementException e){
             throw new RuntimeException("Cannot extract Statement from [" + locateMethod + ":" + locateLine + "].");
         }
-    }
-
-    /**
-     * exprから次に探索の対象となる変数の名前を取得する。
-     * exprの演算に直接用いられている変数のみが対象で、引数やメソッド呼び出しの対象となる変数は除外する。
-     * @return 変数名のリスト
-     */
-    static List<String> extractNeighborVariableNames(Expression expr, boolean includeIndirectUsedVariable){
-        return expr.findAll(NameExpr.class).stream()
-                //引数やメソッド呼び出しに用いられる変数を除外
-                .filter(nameExpr -> includeIndirectUsedVariable || nameExpr.findAncestor(MethodCallExpr.class).isEmpty())
-                .map(NameExpr::toString)
-                .collect(Collectors.toList());
     }
 }
