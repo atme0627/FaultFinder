a5bb07a87d99a97174f289392190bda1be83f17e
atme627 <ezaki.m.aa@m.titech.ac.jp>
2026-01-13 16:47:31 +0900

    `ValueAtSuspiciousExpressionTracer` に `TraceValueAtSuspiciousExpressionStrategy` をフィールドとして導入し、`traceAll` メソッドをリファクタリングしてロジックを整理

--- src/main/java/jisd/fl/core/domain/ValueAtSuspiciousExpressionTracer.java
+++ src/main/java/jisd/fl/core/domain/ValueAtSuspiciousExpressionTracer.java
@@ -1,5 +1,6 @@
 package jisd.fl.core.domain;
 
+import jisd.fl.core.domain.port.TraceValueAtSuspiciousExpressionStrategy;
 import jisd.fl.infra.jdi.JDITraceValueAtSuspiciousArgumentStrategy;
 import jisd.fl.infra.jdi.JDITraceValueAtSuspiciousAssignmentStrategy;
 import jisd.fl.infra.jdi.JDITraceValueAtSuspiciousReturnValueStrategy;
@@ -12,13 +13,21 @@ public class ValueAtSuspiciousExpressionTracer {
      * 複数回SuspiciousExpressionが実行されているときは、最後に実行された時の値を使用する
      * @return
      */
-    public TracedValueCollection traceAllAtSuspiciousExpression(SuspiciousExpression suspExpr){
+
+    private final TraceValueAtSuspiciousExpressionStrategy assignmentStrategy;
+    private final TraceValueAtSuspiciousExpressionStrategy returnValueStrategy;
+    private final TraceValueAtSuspiciousExpressionStrategy argumentStrategy;
+
+    public ValueAtSuspiciousExpressionTracer(){
+        assignmentStrategy = new JDITraceValueAtSuspiciousAssignmentStrategy();
+        returnValueStrategy = new JDITraceValueAtSuspiciousReturnValueStrategy();
+        argumentStrategy = new JDITraceValueAtSuspiciousArgumentStrategy();
+    }
+    public TracedValueCollection traceAll(SuspiciousExpression suspExpr){
         return switch (suspExpr) {
-            case SuspiciousAssignment thisSuspAssign ->
-                    JDITraceValueAtSuspiciousAssignmentStrategy.traceAllValuesAtSuspExpr(thisSuspAssign);
-            case SuspiciousReturnValue thisSuspReturn ->
-                    JDITraceValueAtSuspiciousReturnValueStrategy.traceAllValuesAtSuspExpr(thisSuspReturn);
-            case SuspiciousArgument thisSuspArg -> JDITraceValueAtSuspiciousArgumentStrategy.traceAllValuesAtSuspExpr(thisSuspArg);
+            case SuspiciousAssignment suspAssign -> assignmentStrategy.traceAllValuesAtSuspExpr(suspAssign);
+            case SuspiciousReturnValue suspReturn -> returnValueStrategy.traceAllValuesAtSuspExpr(suspReturn);
+            case SuspiciousArgument suspArg -> argumentStrategy.traceAllValuesAtSuspExpr(suspArg);
             default -> throw new IllegalStateException("Unexpected value: " + suspExpr);
         };
     }
