f87bdc3683d97557a659a70b204b6c601f7555bd
atme627 <ezaki.m.aa@m.titech.ac.jp>
2026-01-13 16:09:48 +0900

    JDISearchSuspiciousReturnsReturnValueStrategy内に検索ロジックを統合し、JDISuspReturnのsearchSuspiciousReturnsメソッドを削除してコードを整理

--- src/main/java/jisd/fl/infra/jdi/JDISearchSuspiciousReturnsReturnValueStrategy.java
+++ src/main/java/jisd/fl/infra/jdi/JDISearchSuspiciousReturnsReturnValueStrategy.java
@@ -1,13 +1,128 @@
 package jisd.fl.infra.jdi;
 
+import com.sun.jdi.IncompatibleThreadStateException;
+import com.sun.jdi.StackFrame;
+import com.sun.jdi.ThreadReference;
+import com.sun.jdi.event.Event;
+import com.sun.jdi.event.EventSet;
+import com.sun.jdi.event.MethodExitEvent;
+import com.sun.jdi.event.StepEvent;
+import com.sun.jdi.request.EventRequestManager;
+import com.sun.jdi.request.MethodExitRequest;
+import com.sun.jdi.request.StepRequest;
+import jisd.debug.EnhancedDebugger;
 import jisd.fl.core.domain.port.SearchSuspiciousReturnsStrategy;
+import jisd.fl.core.domain.port.SuspiciousExpressionFactory;
+import jisd.fl.core.entity.MethodElementName;
+import jisd.fl.infra.javaparser.JavaParserSuspiciousExpressionFactory;
 import jisd.fl.probe.info.*;
+import jisd.fl.util.TestUtil;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class JDISearchSuspiciousReturnsReturnValueStrategy implements SearchSuspiciousReturnsStrategy {
+    static final SuspiciousExpressionFactory factory = new JavaParserSuspiciousExpressionFactory();
     @Override
     public List<SuspiciousReturnValue> search(SuspiciousExpression suspExpr) {
-        return JDISuspReturn.searchSuspiciousReturns((SuspiciousReturnValue) suspExpr);
+        SuspiciousReturnValue suspReturn = (SuspiciousReturnValue) suspExpr;
+        final List<SuspiciousReturnValue> result = new ArrayList<>();
+        if(!(suspReturn).hasMethodCalling()) return result;
+
+        //Debugger生成
+        String main = TestUtil.getJVMMain((suspReturn).failedTest);
+        String options = TestUtil.getJVMOption();
+        EnhancedDebugger eDbg = new EnhancedDebugger(main, options);
+        //ブレークポイントにヒットした時に行う処理を定義
+        //ここではその行で呼ばれてるメソッド情報を抽出
+        EnhancedDebugger.BreakpointHandler handler = (vm, bpe) -> {
+            //既に情報が取得できている場合は終了
+            if(!result.isEmpty()) return;
+
+            List<SuspiciousReturnValue> resultCandidate = new ArrayList<>();
+            EventRequestManager manager = vm.eventRequestManager();
+
+            //このスレッドでの MethodExit を記録するリクエストを作成
+            ThreadReference thread = bpe.thread();
+            MethodExitRequest meReq = EnhancedDebugger.createMethodExitRequest(manager, thread);
+            //この行の実行が終わったことを検知するステップリクエストを作成
+            //この具象クラスではステップイベントの通知タイミングで、今調査していた行が調べたい行だったかを確認
+            StepRequest stepReq = EnhancedDebugger.createStepOutRequest(manager, thread);
+
+            //直前に通知されたMethodExitEventを保持
+            //StepEventでreturnから返った時にこのMEEを使ってreturnのactualValueを手にいれる。
+            MethodExitEvent recentMee = null;
+
+            // ブレークポイント地点でのコールスタックの深さを取得
+            // 呼び出しメソッドの取得条件を 深さ == depthBeforeCall + 1　にすることで
+            // 再帰呼び出し含め、その行で直接呼ばれたメソッドのみ取ってこれる
+            int depthBeforeCall = TmpJDIUtils.getCallStackDepth(thread);
+            //一旦 resume して、内部ループで MethodExit／Step を待つ
+            vm.resume();
+            boolean done = false;
+            while (!done) {
+                EventSet es = vm.eventQueue().remove();
+                for (Event ev : es) {
+                    //実行された、とあるメソッドから抜けた
+                    if (ev instanceof MethodExitEvent) {
+                        MethodExitEvent mee = (MethodExitEvent) ev;
+                        recentMee = mee;
+                        StackFrame caller = null;
+                        try {
+                            //thread()がsuspendされていないと例外を投げる
+                            //普通は成功するはず
+                            //waitForThreadPreparation(mee.thread());
+                            caller = mee.thread().frame(1);
+                        } catch (IncompatibleThreadStateException e) {
+                            throw new RuntimeException("Target thread must be suspended.");
+                        }
+
+                        //収集するのは指定した行で直接呼び出したメソッドのみ
+                        //depthBeforeCallとコールスタックの深さを比較することで直接呼び出したメソッドかどうかを判定
+                        if (mee.thread().equals(thread) && TmpJDIUtils.getCallStackDepth(mee.thread()) == depthBeforeCall + 1) {
+                            MethodElementName invokedMethod = new MethodElementName(EnhancedDebugger.getFqmn(mee.method()));
+                            int locateLine = mee.location().lineNumber();
+                            String actualValue = TmpJDIUtils.getValueString(mee.returnValue());
+                            try {
+                                SuspiciousReturnValue newSuspReturn = factory.createReturnValue(
+                                        suspReturn.failedTest,
+                                        invokedMethod,
+                                        locateLine,
+                                        actualValue
+                                );
+                                resultCandidate.add(newSuspReturn);
+                            } catch (RuntimeException e) {
+                                System.out.println("cannot create SuspiciousReturnValue: " + e.getMessage() + " at " + invokedMethod + " line:" + locateLine);
+                            }
+                        }
+                        vm.resume();
+                    }
+                    //調査対象の行の実行が終了
+                    //ここで、調査した行が目的のものであったかチェック
+                    if (ev instanceof StepEvent) {
+                        done = true;
+                        StepEvent se = (StepEvent) ev;
+                        if(recentMee == null){
+                            //ここには到達しないはず
+                            throw new RuntimeException("Something is wrong.");
+                        }
+                        if(TmpJDIUtils.validateIsTargetExecution(recentMee, suspReturn.actualValue)) result.addAll(resultCandidate);
+                        //vmをresumeしない
+                    }
+                }
+            }
+            //動的に作ったリクエストを無効化
+            meReq.disable();
+            stepReq.disable();
+        };
+
+        //VMを実行し情報を収集
+        eDbg.handleAtBreakPoint((suspReturn).locateMethod.getFullyQualifiedClassName(), (suspReturn).locateLine, handler);
+        if(result.isEmpty()){
+            System.err.println("[[searchSuspiciousReturns]] Could not confirm [ "
+                    + "(return value) == " + (suspReturn).actualValue
+                    + " ] on " + (suspReturn).locateMethod + " line:" + (suspReturn).locateLine);
+        }
+        return result;
     }
 }
