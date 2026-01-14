48461384d7499c5f32810c889a3dd44f438a73ec
atme627 <ezaki.m.aa@m.titech.ac.jp>
2026-01-15 01:01:51 +0900

    `EvalOrderVisitor` を `infra.javaparser` パッケージに移動し、`StatementEvalOrderVisitor` にリネーム。関連コードをリファクタリングして整理

--- src/main/java/jisd/fl/probe/info/JavaParserSuspArg.java
+++ src/main/java/jisd/fl/probe/info/JavaParserSuspArg.java
@@ -31,37 +31,4 @@ public class JavaParserSuspArg {
         throw new RuntimeException("Something is wrong. (stmt: " + stmt + ", callCountAfterTargetInLine: " + callCountAfterTargetInLine + ", argIndex: " + argIndex + ", calleeMethodName: " + calleeMethodName + " ) ");
     }
 
-    /**
-     * Java の実行時評価順で MethodCallExpr を収集する Visitor
-     */
-    private static class EvalOrderVisitor extends VoidVisitorAdapter<List<Expression>> {
-        @Override
-        public void visit(MethodCallExpr mce, List<Expression> collector) {
-            // 1) レシーバ（scope）があれば先に評価
-            mce.getScope().ifPresent(scope -> scope.accept(this, collector));
-            // 2) 引数を左から順に評価
-            for (Expression arg : mce.getArguments()) {
-                arg.accept(this, collector);
-            }
-            // 3) 最後に「呼び出しイベント」として自分自身を追加
-            collector.add(mce);
-        }
-
-        @Override
-        public void visit(ObjectCreationExpr oce, List<Expression> collector) {
-            // 1) コンストラクタのスコープ（new Outer.Inner() の Outer など）がある場合は先に評価
-            oce.getScope().ifPresent(scope -> scope.accept(this, collector));
-            // 2) 引数を左から順に評価
-            for (Expression arg : oce.getArguments()) {
-                arg.accept(this, collector);
-            }
-            // 3) 匿名クラスボディ内にある式（必要なら追加）
-            if (oce.getAnonymousClassBody().isPresent()) {
-                oce.getAnonymousClassBody().get().forEach(body -> body.accept(this, collector));
-            }
-
-            // 3) 最後に「呼び出しイベント」として自分自身を追加
-            collector.add(oce);
-        }
-    }
 }
