0eb7e5846c92508b6dfad4211ae3ef29996a03c1
atme627 <ezaki.m.aa@m.titech.ac.jp>
2026-01-15 13:42:00 +0900

    `不要な TracedValueCollection のインポートを削除し、変数名を整理`

--- src/main/java/jisd/fl/core/domain/CauseLineFinder.java
+++ src/main/java/jisd/fl/core/domain/CauseLineFinder.java
@@ -12,7 +12,6 @@ import jisd.fl.infra.jdi.TargetVariableTracer;
 import jisd.fl.core.entity.susp.SuspiciousExpression;
 import jisd.fl.core.entity.susp.SuspiciousVariable;
 import jisd.fl.probe.record.TracedValue;
-import jisd.fl.probe.record.TracedValueCollection;
 import jisd.fl.util.analyze.StaticAnalyzer;
 
 import java.nio.file.NoSuchFileException;
@@ -59,10 +58,10 @@ public class CauseLineFinder {
      *
      * @return
      */
-    private Optional<SuspiciousExpression> searchProbeLine(List<TracedValue> watchedValues) {
+    private Optional<SuspiciousExpression> searchProbeLine(List<TracedValue> tracedValues) {
         /* 1a. すでに定義されていた変数に代入が行われたパターン */
         //代入の実行後にactualの値に変化している行の特定(ない場合あり)
-        List<TracedValue> changeToActualLines = valueChangedToActualLine(watchedValues, target.getActualValue());
+        List<TracedValue> changeToActualLines = valueChangedToActualLine(tracedValues, target.getActualValue());
         //代入の実行後にactualの値に変化している行あり -> その中で最後に実行された行がprobe line
         if (!changeToActualLines.isEmpty()) {
             //原因行
@@ -87,7 +86,7 @@ public class CauseLineFinder {
     }
 
 
-    private List<TracedValue> valueChangedToActualLine(List<TracedValue> watchedValues, String actual) {
+    private List<TracedValue> valueChangedToActualLine(List<TracedValue> tracedValues, String actual) {
         //対象の変数に値の変化が起きている行の特定
         List<Integer> assignedLine = TmpJavaParserUtils.valueChangingLine(target);
         //対象の変数を定義している行を追加
@@ -100,12 +99,12 @@ public class CauseLineFinder {
         );
 
         List<TracedValue> changedToActualLines = new ArrayList<>();
-        for (int i = 0; i < watchedValues.size() - 1; i++) {
-            TracedValue watchingLine = watchedValues.get(i);
+        for (int i = 0; i < tracedValues.size() - 1; i++) {
+            TracedValue watchingLine = tracedValues.get(i);
             //watchingLineでは代入が行われていない -> 原因行ではない
             if (!assignedLine.contains(watchingLine.lineNumber)) continue;
             //次の行で値がactualに変わっている -> その行が原因行の候補
-            TracedValue afterAssignLine = watchedValues.get(i + 1);
+            TracedValue afterAssignLine = tracedValues.get(i + 1);
             if (afterAssignLine.value.equals(actual)) changedToActualLines.add(watchingLine);
         }
         changedToActualLines.sort(TracedValue::compareTo);
