4c7e37805c56cf7b807249d64dee1bf580138df6
atme627 <ezaki.m.aa@m.titech.ac.jp>
2026-01-15 13:25:29 +0900

    `TracedValuesOfTarget` の不要なコードを削除して整理

--- src/main/java/jisd/fl/probe/record/TracedValuesOfTarget.java
+++ src/main/java/jisd/fl/probe/record/TracedValuesOfTarget.java
@@ -4,21 +4,12 @@ import jisd.debug.Location;
 import jisd.debug.value.ValueInfo;
 import jisd.fl.core.entity.susp.SuspiciousVariable;
 
-import java.time.LocalDateTime;
-import java.util.Collection;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 //viで指定された変数のみ記録
 //原因行特定のために使用
 public class TracedValuesOfTarget extends TracedValueCollection {
     final SuspiciousVariable target;
-
-    public TracedValuesOfTarget(SuspiciousVariable target, List<ValueInfo> valuesOfTarget, Map<LocalDateTime, Location> locationAtTime) {
-        this.target = target;
-        this.record = convertValuesOfTarget(valuesOfTarget, locationAtTime);
-    }
 
     private TracedValuesOfTarget(List<TracedValue> record, SuspiciousVariable target){
         super(record);
@@ -26,13 +17,6 @@ public class TracedValuesOfTarget extends TracedValueCollection {
     }
     public static TracedValueCollection of(List<TracedValue> record, SuspiciousVariable target){
         return new TracedValuesOfTarget(record, target);
-    }
-
-    private List<TracedValue> convertValuesOfTarget(List<ValueInfo> valuesOfTarget, Map<LocalDateTime, Location> locationAtTime){
-        return  valuesOfTarget.stream()
-                        .map(v -> convertValueInfo(v, locationAtTime.get(v.getCreatedAt())))
-                        .flatMap(Collection::stream)
-                        .collect(Collectors.toList());
     }
 
     @Override
