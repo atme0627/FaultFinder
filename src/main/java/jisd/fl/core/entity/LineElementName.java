5d10e777bf1d8fc9a8ab70c687457ddb3d7693ca
atme627 <ezaki.m.aa@m.titech.ac.jp>
2026-01-18 11:07:00 +0900

    `LineElementName クラスをリファクタリングし、未使用メソッドを削除してコードを簡素化`

--- src/main/java/jisd/fl/core/entity/LineElementName.java
+++ src/main/java/jisd/fl/core/entity/LineElementName.java
@@ -1,5 +1,6 @@
 package jisd.fl.core.entity;
-import java.nio.file.Path;
+
+import java.util.Objects;
 
 public class LineElementName implements CodeElementIdentifier {
     private final MethodElementName methodElementName;
@@ -15,81 +16,41 @@ public class LineElementName implements CodeElementIdentifier {
         this.line = line;
     }
 
-    public String getFullyQualifiedClassName() {
+    @Override
+    public String fullyQualifiedClassName() {
         return methodElementName.fullyQualifiedClassName();
     }
 
     @Override
-    public String getFullyQualifiedMethodName() {
-        return methodElementName.fullyQualifiedName();
+    public String fullyQualifiedName() {
+        return methodElementName.fullyQualifiedName() + " line: " + line;
     }
 
-    public String getShortMethodName() {
-        return methodElementName.shortMethodName();
-    }
 
     @Override
-    public Path getFilePath() {
-        return methodElementName.getFilePath();
+    public String compressedName() {
+        return methodElementName.compressedMethodName() + " line: " + line;
     }
 
-    @Override
-    public Path getFilePath(boolean isTest) {
-        return methodElementName.getFilePath(isTest);
-    }
 
-    @Override
-    public String compressedShortMethodName() {
-        return methodElementName.compressedShortMethodName() + " line: " + line;
-    }
-
-    @Override
-    public boolean isNeighbor(CodeElementIdentifier target){
-        if(!(target instanceof LineElementName lineElementTarget)) return false;
-        return this.methodElementName.equals(lineElementTarget.methodElementName);
-    }
-
-    @Override
-    public int compareTo(CodeElementIdentifier o) {
-        if(o instanceof LineElementName) {
-            if (this.getFullyQualifiedMethodName().equals(o.getFullyQualifiedMethodName())) {
-                return Integer.compare(this.line, ((LineElementName) o).line);
-            }
-        }
-        return this.getFullyQualifiedMethodName().compareTo(o.getFullyQualifiedMethodName());
+    public int compareTo(LineElementName o) {
+        return (!methodElementName.equals(o.methodElementName)) ? methodElementName.compareTo(o.methodElementName) : line - o.line;
     }
 
     @Override
     public boolean equals(Object obj){
         if(obj == null) return false;
-        if(!(obj instanceof LineElementName)) return false;
-        return this.getFullyQualifiedMethodName()
-                .equals(((LineElementName) obj).getFullyQualifiedMethodName())
-                && this.line == ((LineElementName) obj).line;
+        if(!(obj instanceof LineElementName e)) return false;
+        return this.methodElementName.equals(e.methodElementName) && this.line == e.line;
     }
 
     @Override
     public int hashCode(){
-        return this.getFullyQualifiedMethodName().hashCode() + line;
+        return Objects.hash(methodElementName, line);
     }
 
     @Override
     public String toString(){
-        return this.getFullyQualifiedMethodName() + " line: " + line;
-    }
-
-    public int getLine(){
-        return line;
-    }
-
-    public String compressedClassName(){
-        StringBuilder shortClassName = new StringBuilder();
-        String[] packages = getFullyQualifiedClassName().split("\\.");
-        for(int j = 0; j < packages.length; j++){
-            String packageName = j < packages.length - 2 ? String.valueOf(packages[j].charAt(0)) : packages[j];
-            shortClassName.append(packageName);
-            if(j < packages.length - 1) shortClassName.append(".");
-        }
-        return shortClassName.toString();
+        return this.fullyQualifiedName();
     }
 }
