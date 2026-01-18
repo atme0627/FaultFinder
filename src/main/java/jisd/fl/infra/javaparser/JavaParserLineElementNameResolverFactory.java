4917eee58e3cf0af1c9c5d753962ff007803cb8f
atme627 <ezaki.m.aa@m.titech.ac.jp>
2026-01-18 17:40:51 +0900

    `JavaParserLineElementNameResolverFactory#create メソッドの引数から行数情報を削除し、クラス情報から行数を動的取得するよう変更`

--- src/main/java/jisd/fl/infra/javaparser/JavaParserLineElementNameResolverFactory.java
+++ src/main/java/jisd/fl/infra/javaparser/JavaParserLineElementNameResolverFactory.java
@@ -13,13 +13,13 @@ public class JavaParserLineElementNameResolverFactory {
      * 対象クラスの情報と行数を基にして、LineElementNameResolverインスタンスを生成する。
      *
      * @param targetClass 対象となるクラスを表すClassElementNameオブジェクト
-     * @param lines 対象クラスの最大行数
      * @return 生成されたLineElementNameResolverインスタンス
      */
-    public static LineElementNameResolver create(ClassElementName targetClass, int lines) throws NoSuchFileException {
+    public static LineElementNameResolver create(ClassElementName targetClass) throws NoSuchFileException {
+        // 対象クラスの最大行数
+        int lines = JavaParserUtils.parseClass(targetClass).getEnd().get().line;
         MethodElementName clinit = new MethodElementName(targetClass.fullyQualifiedClassName() + "#<clinit>()");
         LineElementNameResolver resolver = new LineElementNameResolver(lines, clinit);
-
         for(CallableDeclaration<?> cd : JavaParserUtils.extractNode(targetClass, CallableDeclaration.class)){
             if(cd.getRange().isPresent()) continue;
             Range methodRange = cd.getRange().get();
