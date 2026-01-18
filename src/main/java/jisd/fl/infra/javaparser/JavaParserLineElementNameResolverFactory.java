a9845ccf57689d803b10445ce21447dbfec507ac
atme627 <ezaki.m.aa@m.titech.ac.jp>
2026-01-18 17:33:42 +0900

    `JavaParserLineElementNameResolverFactory クラスを追加し、対象クラスの行数とメソッド要素名の対応管理機能を実装`

---
+++ src/main/java/jisd/fl/infra/javaparser/JavaParserLineElementNameResolverFactory.java
@@ -0,0 +1,31 @@
+package jisd.fl.infra.javaparser;
+
+import com.github.javaparser.Range;
+import com.github.javaparser.ast.body.CallableDeclaration;
+import jisd.fl.core.entity.element.ClassElementName;
+import jisd.fl.core.entity.element.LineElementNameResolver;
+import jisd.fl.core.entity.element.MethodElementName;
+
+import java.nio.file.NoSuchFileException;
+
+public class JavaParserLineElementNameResolverFactory {
+    /**
+     * 対象クラスの情報と行数を基にして、LineElementNameResolverインスタンスを生成する。
+     *
+     * @param targetClass 対象となるクラスを表すClassElementNameオブジェクト
+     * @param lines 対象クラスの最大行数
+     * @return 生成されたLineElementNameResolverインスタンス
+     */
+    public static LineElementNameResolver create(ClassElementName targetClass, int lines) throws NoSuchFileException {
+        MethodElementName clinit = new MethodElementName(targetClass.fullyQualifiedClassName() + "#<clinit>()");
+        LineElementNameResolver resolver = new LineElementNameResolver(lines, clinit);
+
+        for(CallableDeclaration<?> cd : JavaParserUtils.extractNode(targetClass, CallableDeclaration.class)){
+            if(cd.getRange().isPresent()) continue;
+            Range methodRange = cd.getRange().get();
+            MethodElementName method = new MethodElementName(targetClass.fullyQualifiedClassName() + "#" + cd.getSignature());
+            resolver.putMethodRange(methodRange.begin.line, methodRange.end.line, method);
+        }
+        return resolver;
+    }
+}
