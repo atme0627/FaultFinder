58d22d32d3a83979f693457435f3faebc18aef11
atme627 <ezaki.m.aa@m.titech.ac.jp>
2026-01-18 18:19:00 +0900

    `ClassSbflCoverage クラスを追加し、行、メソッド、クラス単位でのカバレッジ情報収集機能を実装`

--- src/main/java/jisd/fl/core/entity/coverage/ClassSbflCoverage.java
+++ src/main/java/jisd/fl/core/entity/coverage/ClassSbflCoverage.java
@@ -1,4 +1,134 @@
 package jisd.fl.core.entity.coverage;
 
+import jisd.fl.core.entity.element.ClassElementName;
+import jisd.fl.core.entity.element.LineElementName;
+import jisd.fl.core.entity.element.LineElementNameResolver;
+import jisd.fl.core.entity.element.MethodElementName;
+import jisd.fl.infra.javaparser.JavaParserLineElementNameResolverFactory;
+import org.jacoco.core.analysis.IClassCoverage;
+import org.jacoco.core.analysis.ICounter;
+import org.jacoco.core.analysis.IMethodCoverage;
+
+import java.nio.file.NoSuchFileException;
+import java.util.Arrays;
+
 public class ClassSbflCoverage {
+
+    private final ClassElementName targetClass;
+    private final LineElementNameResolver lineElementNameResolver;
+
+    private int totalPass = 0;
+    private int totalFail = 0;
+
+    private final ElementIDRegistry<LineElementName> lineIds = new ElementIDRegistry<>();
+    private final SbflCountsTable lineCounts = new SbflCountsTable();
+
+    private final ElementIDRegistry<MethodElementName> methodIds = new ElementIDRegistry<>();
+    private final SbflCountsTable methodCounts = new SbflCountsTable();
+
+    private int classEp = 0;
+    private int classEf = 0;
+
+    //--- for optimize ---
+    private boolean initialized = false;
+    private int firstLIne = -1;
+    private int lastLine = -1;
+
+    private int[] lineIdByLine;
+    private int[] methodIdByLine;
+    //--------------------
+
+
+    public ClassSbflCoverage(ClassElementName targetClass) throws NoSuchFileException {
+        this.targetClass = targetClass;
+        this.lineElementNameResolver = JavaParserLineElementNameResolverFactory.create(targetClass);
+    }
+    public int totalPass() {return totalPass;}
+    public int totalFail() {return totalFail;}
+
+    public void accept(IClassCoverage cc, boolean testPassed) {
+        initializeIfNeeded(cc);
+        if(testPassed) totalPass++; else totalFail++;
+
+        //行単位カバレッジ
+        for(int line = firstLIne; line <= lastLine; line++){
+            int stat = cc.getLine(line).getStatus();
+            if(stat == ICounter.EMPTY) continue;
+
+            boolean executed = (stat != ICounter.NOT_COVERED);
+            if(!executed) continue;
+            int id = lineIdByLine[line];
+            if(id < 0){
+                System.err.println("[INITIALIZE MAY FAILED] target: " + targetClass + ", lineIdByLine[" + line + "] = " + id);
+                continue;
+            }
+            if(testPassed) lineCounts.incEp(id); else lineCounts.incEf(id);
+        }
+
+        //メソッド単位カバレッジ
+        for(IMethodCoverage mc : cc.getMethods()){
+            boolean executed = mc.getMethodCounter().getCoveredCount() > 0;
+            if(!executed) continue;
+
+            int fl = mc.getFirstLine();
+            if(fl <= 0 || fl > lastLine) continue;
+
+            int id = methodIdByLine[fl];
+            if(id < 0){
+                System.err.println("[INITIALIZE MAY FAILED] target: " + targetClass + ", methodIdByLine[" + fl + "] = " + id);
+                continue;
+            }
+            if(testPassed) methodCounts.incEp(id); else methodCounts.incEf(id);
+        }
+
+        //クラス単位カバレッジ
+        boolean executed = isClassExecuted(cc);
+        if(executed) {
+            if (testPassed) classEp++;
+            else classEf++;
+        }
+    }
+
+    private static boolean isClassExecuted(IClassCoverage cc) {
+        int first = cc.getFirstLine();
+        int last = cc.getLastLine();
+        for(int line = first; line <= last; line++){
+            int stat = cc.getLine(line).getStatus();
+            if(stat == ICounter.PARTLY_COVERED || stat == ICounter.FULLY_COVERED) return true;
+        }
+        return false;
+    }
+
+    private void initializeIfNeeded(IClassCoverage cc){
+        if(initialized) return;
+        this.firstLIne = cc.getFirstLine();
+        this.lastLine = cc.getLastLine();
+
+        lineIdByLine = new int[lastLine + 1];
+        methodIdByLine = new int[lastLine + 1];
+        Arrays.fill(lineIdByLine, -1);
+        Arrays.fill(methodIdByLine, -1);
+
+        //行単位要素の初期化
+        for(int line = firstLIne; line <= lastLine; line++){
+            int stat = cc.getLine(line).getStatus();
+            if(stat == ICounter.EMPTY) continue; // 実行不可能行 or 範囲外
+
+            LineElementName e = lineElementNameResolver.lineElementAt(line);
+            int id = lineIds.getOrCreate(e);
+            lineIdByLine[line] = id;
+        }
+        lineCounts.ensureCapacity(lineIds.size());
+
+        //メソッド単位要素の初期化
+        for(IMethodCoverage mc : cc.getMethods()){
+            int fl = mc.getFirstLine();
+            if(fl <= 0 || fl > lastLine) continue;
+            MethodElementName e = lineElementNameResolver.lineElementAt(fl).methodElementName;
+            int id = methodIds.getOrCreate(e);
+            methodIdByLine[fl] = id;
+        }
+        methodCounts.ensureCapacity(methodIds.size());
+        initialized = true;
+    }
 }
