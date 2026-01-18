7fdd398c9f7d27cb02b447e0702392262175cff5
atme627 <ezaki.m.aa@m.titech.ac.jp>
2026-01-19 00:43:28 +0900

    `JsonIO クラスから未使用の importCoverage メソッドと関連デシリアライザを削除し、コードを簡素化`

--- src/main/java/jisd/fl/util/JsonIO.java
+++ src/main/java/jisd/fl/util/JsonIO.java
@@ -1,13 +1,7 @@
 package jisd.fl.util;
 
-import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.KeyDeserializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.module.SimpleModule;
-import jisd.fl.sbfl.coverage.CoverageCollection;
-import jisd.fl.core.entity.element.CodeElementIdentifier;
-import jisd.fl.core.entity.element.MethodElementName;
 import org.json.JSONObject;
 
 import java.io.File;
@@ -22,39 +16,6 @@ public class JsonIO {
             mapper.writeValue(output, obj);
         } catch (Exception e) {
             throw new RuntimeException("Failed to export to JSON", e);
-        }
-    }
-
-    public static CoverageCollection importCoverage(File input){
-        ObjectMapper mapper = new ObjectMapper();
-        SimpleModule module = new SimpleModule();
-        module.addKeyDeserializer(CodeElementIdentifier.class,
-                new CodeElementNameKeyDeserializer());
-        mapper.registerModule(module);
-
-        try {
-            return mapper.readValue(input, CoverageCollection.class);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static class CodeElementNameKeyDeserializer extends KeyDeserializer {
-        @Override
-        public CodeElementIdentifier deserializeKey(String key, DeserializationContext ctxt) {
-            // key は toString() の結果
-            // 例: "com.example.Foo#bar() line: 42"
-            if(key.contains("line")) {
-                String[] parts = key.split(" line: ");
-                String fqmnWithSig = parts[0];            // "com.example.Foo#bar()"
-                int line = Integer.parseInt(parts[1]); //  42
-
-                // MethodElementName／LineElementName を組み立て
-                return new MethodElementName(fqmnWithSig).toLineElementName(line);
-            }
-            else {
-                return new MethodElementName(key);
-            }
         }
     }
 
