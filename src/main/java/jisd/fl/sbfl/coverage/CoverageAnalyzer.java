41d4ff837319d10cf07c7a158314ba6167f29bc8
atme627 <ezaki.m.aa@m.titech.ac.jp>
2026-01-21 14:10:36 +0900

    `CoverageAnalyzer クラスで JacocoTestUtil の依存を削除し、JacocoTestExecClient クラスの listTestMethods メソッドを使用するよう変更`

--- src/main/java/jisd/fl/sbfl/coverage/CoverageAnalyzer.java
+++ src/main/java/jisd/fl/sbfl/coverage/CoverageAnalyzer.java
@@ -1,7 +1,6 @@
 package jisd.fl.sbfl.coverage;
 
 import jisd.fl.core.entity.element.ClassElementName;
-import jisd.fl.infra.jacoco.JacocoTestUtil;
 import jisd.fl.infra.jacoco.ProjectSbflCoverage;
 import jisd.fl.infra.jacoco.exec.JacocoTestExecClient;
 import jisd.fl.infra.jacoco.exec.JacocoTestExecServerHandle;
@@ -9,7 +8,7 @@ import jisd.fl.core.entity.element.MethodElementName;
 import org.jacoco.core.data.ExecutionDataStore;
 
 import java.io.*;
-import java.util.Set;
+import java.util.List;
 
 //テストケースを実行して、jacoco.execファイルを生成するクラス
 public class CoverageAnalyzer {
@@ -20,11 +19,11 @@ public class CoverageAnalyzer {
     }
 
     public void analyze(ClassElementName testClassName){
-        Set<MethodElementName> testMethodNames = JacocoTestUtil.getTestMethods(testClassName);
-        if(testMethodNames.isEmpty()) throw new RuntimeException("test method is not found. [CLASS] " + testMethodNames);
-
         try (var handle = JacocoTestExecServerHandle.startDefault(20000)) {
             JacocoTestExecClient client = handle.client();
+            List<MethodElementName> testMethodNames = client.listTestMethods(testClassName);
+            if(testMethodNames.isEmpty()) throw new RuntimeException("test method is not found. [CLASS] " + testMethodNames);
+
             for(MethodElementName testMethodName : testMethodNames) {
                 JacocoTestExecClient.TestExecReply reply = client.runTest(testMethodName);
                 byte[] coverageData = reply.execBytes();
