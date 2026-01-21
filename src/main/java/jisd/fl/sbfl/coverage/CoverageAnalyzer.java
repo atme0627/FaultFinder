f568be72f317b87a9038b4a91d35db6747bbde49
atme627 <ezaki.m.aa@m.titech.ac.jp>
2026-01-21 14:42:34 +0900

    `JacocoUtil クラスを削除し、CoverageAnalyzer クラスに分析処理を移行`

--- src/main/java/jisd/fl/sbfl/coverage/CoverageAnalyzer.java
+++ src/main/java/jisd/fl/sbfl/coverage/CoverageAnalyzer.java
@@ -7,7 +7,11 @@ import jisd.fl.infra.jacoco.ProjectSbflCoverage;
 import jisd.fl.infra.jacoco.exec.JacocoTestExecClient;
 import jisd.fl.infra.jacoco.exec.JacocoTestExecServerHandle;
 import jisd.fl.core.entity.element.MethodElementName;
+import org.jacoco.core.analysis.Analyzer;
+import org.jacoco.core.analysis.ICoverageVisitor;
+import org.jacoco.core.data.ExecutionData;
 import org.jacoco.core.data.ExecutionDataStore;
+import org.jacoco.core.tools.ExecFileLoader;
 
 import java.io.*;
 import java.util.List;
@@ -39,8 +43,8 @@ public class CoverageAnalyzer {
                 byte[] coverageData = reply.execBytes();
                 visitor.setTestsPassed(reply.passed());
 
-                ExecutionDataStore execData = JacocoUtil.execDataFromBytes(coverageData);
-                JacocoUtil.analyzeExecData(execData, visitor, cache);
+                ExecutionDataStore execData = execDataFromBytes(coverageData);
+                analyzeExecData(execData, visitor, cache);
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -50,6 +54,25 @@ public class CoverageAnalyzer {
     //TODO: 複数テストクラスを計測した際の挙動をテスト
     public ProjectSbflCoverage result(){
         return visitor.getCoverages();
+    }
+
+    private static void analyzeExecData(ExecutionDataStore executionData, ICoverageVisitor cv, ClassFileCache cache) throws IOException {
+        //jacocoによるテスト対象の解析
+        final Analyzer analyzer = new Analyzer(executionData, cv);
+        for(ExecutionData execData : executionData.getContents()) {
+            byte[] classBytes = cache.get(execData.getName());
+            if(classBytes != null) {
+                analyzer.analyzeClass(classBytes, execData.getName());
+            }
+        }
+    }
+
+    private static ExecutionDataStore execDataFromBytes(byte[] execBytes) throws IOException {
+        ExecFileLoader loader = new ExecFileLoader();
+        try (InputStream in = new ByteArrayInputStream(execBytes)) {
+            loader.load(in);
+        }
+        return loader.getExecutionDataStore();
     }
 }
 
