f15381f8c6bb60929baa1c716b7fd69516e3c47d
atme627 <ezaki.m.aa@m.titech.ac.jp>
2026-01-21 17:03:21 +0900

    `Probe クラスから未使用コードを削除し、調査処理を簡素化`

--- src/main/java/jisd/fl/usecase/Probe.java
+++ src/main/java/jisd/fl/usecase/Probe.java
@@ -4,7 +4,6 @@ import jisd.fl.core.domain.NeighborSuspiciousVariablesSearcher;
 import jisd.fl.core.domain.SuspiciousReturnsSearcher;
 import jisd.fl.core.entity.susp.SuspiciousExprTreeNode;
 import jisd.fl.core.entity.susp.SuspiciousExpression;
-import jisd.fl.core.entity.susp.SuspiciousReturnValue;
 import jisd.fl.core.entity.susp.SuspiciousVariable;
 import jisd.fl.core.domain.CauseLineFinder;
 import jisd.fl.presenter.ProbeReporter;
@@ -22,54 +21,6 @@ public class Probe{
         this.reporter = new ProbeReporter();
         this.neighborSearcher = new NeighborSuspiciousVariablesSearcher();
     }
-
-    //調査結果の木構造のルートノードに対応するSuspExprを返す
-//    public SuspiciousExprTreeNode run(int sleepTime) {
-//        Deque<SuspiciousVariable> probingTargets = new ArrayDeque<>();
-//        List<SuspiciousVariable> investigatedTargets = new ArrayList<>();
-//
-//        probingTargets.add(firstTarget);
-//        investigatedTargets.add(firstTarget);
-//
-//        while(!probingTargets.isEmpty()) {
-//            SuspiciousVariable target = probingTargets.removeLast();
-//            reporter.reportProbeTarget(target);
-//            List<SuspiciousExpression> causeExprs = new ArrayList<>();
-//
-//            //search cause line
-//            CauseLineFinder finder = new CauseLineFinder(target);
-//            Optional<SuspiciousExpression> suspExprOpt = finder.find();
-//            if(suspExprOpt.isEmpty()){
-//                System.err.println("[Probe For STATEMENT] Cause line is not found.");
-//                System.err.println("[Probe For STATEMENT] Skip probing");
-//                continue;
-//            }
-//            SuspiciousExpression suspExpr = suspExprOpt.get();
-//            causeExprs.add(suspExpr);
-//            addTreeElement(suspExpr, target);
-//;           reporter.reportCauseExpression(suspExpr);
-//            //include return line of callee method to cause lines
-//
-//            causeExprs.addAll(collectInvokedReturnExpressions(suspExpr));
-//
-//            //search next target
-//            System.out.println(" >>> search next target");
-//            List<SuspiciousVariable> newTargets = new ArrayList<>();
-//            for (SuspiciousExpression ce : causeExprs) {
-//                //SuspExprで観測できる全ての変数
-//                List<SuspiciousVariable> neighbor = neighborSearcher.neighborSuspiciousVariables(false, ce);
-//                neighbor.forEach(sv -> sv.setParent(ce));
-//                neighbor.removeAll(investigatedTargets);
-//                newTargets.addAll(neighbor);
-//            }
-//            newTargets.removeAll(investigatedTargets);
-//
-//            investigatedTargets.addAll(newTargets);
-//            probingTargets.addAll(newTargets);
-//            printProbeExInfoFooter(suspExpr, newTargets);
-//        }
-//        return suspiciousExprTreeRoot;
-//    }
 
     // SuspExpr単位で探索する。
     // 0. ユーザ由来のsuspVarから最初のSuspExprを特定する。
@@ -122,29 +73,7 @@ public class Probe{
         return suspiciousExprTreeRoot;
     }
 
-    private List<SuspiciousExpression> collectInvokedReturnExpressions(SuspiciousExpression targetCauseExpr){
-        List<SuspiciousExpression> result = new ArrayList<>();
-        Deque<SuspiciousExpression> suspExprQueue = new ArrayDeque<>();
-        SuspiciousReturnsSearcher searcher = new SuspiciousReturnsSearcher();
-
-        suspExprQueue.add(targetCauseExpr);
-        while(!suspExprQueue.isEmpty()){
-            SuspiciousExpression target = suspExprQueue.removeFirst();
-            SuspiciousExprTreeNode targetNode = suspiciousExprTreeRoot.find(target);
-            if(targetNode == null) throw new RuntimeException("Target node is not found.");
-
-            List<SuspiciousReturnValue> returnsOfTarget = searcher.search(target);
-            if(!returnsOfTarget.isEmpty()) {
-                targetNode.addChild(returnsOfTarget);
-                suspExprQueue.addAll(returnsOfTarget);
-            }
-            result.addAll(returnsOfTarget);
-        }
-        reporter.reportInvokedReturnExpression(suspiciousExprTreeRoot.find(targetCauseExpr));
-        return result;
-    }
-
-    //TODO: Exprが直接呼び出しているmethodのreturnのみ返すようにする。
+    //Exprが直接呼び出しているmethodのreturnのみ返す。
     // int result = calc(a, b);
     //  -> return add(a, b); <= ここだけ返す。
     //    -> return a + b;       <= ここは[return add(a, b)]の探索で探す。
