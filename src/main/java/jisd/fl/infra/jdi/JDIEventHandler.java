475f0f4ea16148a067376af938f78d2fc245db0f
atme627 <ezaki.m.aa@m.titech.ac.jp>
2026-01-27 17:05:26 +0900

    fix: JDIEventHandler の修正および VirtualMachine のインポート箇所を更新

---
+++ src/main/java/jisd/fl/infra/jdi/JDIEventHandler.java
@@ -0,0 +1,8 @@
+package jisd.fl.infra.jdi;
+
+import com.sun.jdi.VirtualMachine;
+import com.sun.jdi.event.Event;
+
+public interface JDIEventHandler<T extends Event> {
+    void handle(VirtualMachine vm, Event event);
+}
